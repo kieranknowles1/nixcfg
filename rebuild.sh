bc6a473963d3854f0d0faddd119c89cd92418bb0
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-22 23:36:59 +0100

    Check that rebuild is not run as root

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$EUID" -eq 0 ]; then
+  echo "Do not run this script as root" >&2
+  exit 1
+fi
+
+# TODO: Git commit on success. Include generation number
+
 sudo nixos-rebuild switch --flake .#desktop
