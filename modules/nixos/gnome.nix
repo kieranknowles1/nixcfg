963b3f476927ed2e292c3937fa43adb3d5ccec11
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-23 00:17:01 +0100

    Remove a bunch of packages

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -6,4 +6,21 @@
   services.xserver.excludePackages = with pkgs; [
     xterm
   ];
+
+  # TODO: Make sure this is sorted and explain why each is unneeded
+  environment.gnome.excludePackages = with pkgs; [
+    evince
+    gnome.cheese # TODO: Why isnt this removed
+    gnome.simple-scan
+    epiphany
+    gnome-connections
+    gnome.gnome-calendar
+    gnome.file-roller
+    gnome.gnome-clocks
+    gnome-tour
+    gnome.gnome-contacts
+    gnome.gnome-maps
+    gnome.yelp
+    gnome.gnome-weather
+  ];
 }
