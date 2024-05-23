dd99d0d04ba9572e3c8235a408759ad300b285fe
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-23 12:52:03 +0100

    Dark > black Small > huge

--- modules/nixos/theme.nix
+++ modules/nixos/theme.nix
@@ -2,8 +2,14 @@
 { pkgs, ... }:
 {
   stylix = {
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-black.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-gray.yaml";
     image = ../../media/wallpaper.jpg;
+
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 16;
+    };
   };
 
   home-manager.sharedModules = [{
