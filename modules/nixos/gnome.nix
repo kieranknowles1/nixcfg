def6e04621c3a7a0dbc9bb611c6b52b337caa997
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-23 18:52:28 +0100

    78: move rest of gnome
    Generation  Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    78 current  2024-05-23 17:35:00  23.11.20240522.4639777   6.1.91                          *

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -3,7 +3,9 @@
 # TODO: Make the link clickable
 { pkgs, ...}:
 {
-  # TODO: Move rest of GNOME here
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Remove unneeded packages
   services.xserver.excludePackages = with pkgs; [
