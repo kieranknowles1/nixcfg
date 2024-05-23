b57929b36668c86dcd8f0b564e3bb8002a4851ee
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-23 13:31:48 +0100

    53: Commit after rebuild
    Generation  Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    53 current  2024-05-23 13:22:43  23.11.20240520.a8695cb   6.1.91                          *

--- rebuild.sh
+++ rebuild.sh
@@ -2,11 +2,25 @@
 
 set -e
 
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <commit message>" >&2
+  exit 1
+fi
+
 if [ "$EUID" -eq 0 ]; then
   echo "Do not run this script as root" >&2
   exit 1
 fi
 
-# TODO: Git commit on success. Include generation number
+commit_message="$1"
 
 sudo nixos-rebuild switch --flake .#desktop
+
+# Header and current generation
+generation_meta=$(nixos-rebuild list-generations | head -n 2)
+
+# Generation number
+generation_number=$(echo "$generation_meta" | tail -n 1 | awk '{print $1}')
+
+git add .
+git commit -m "$generation_number: $commit_message" -m "$generation_meta"
