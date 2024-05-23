7eadd16a7b6d3a77b96f7f0a37931cb3e6b20fb9
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-23 14:28:26 +0100

    58: Disable hot corner
    Generation  Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    58 current  2024-05-23 14:28:21  23.11.20240522.4639777   6.1.91                          *

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -9,6 +9,11 @@
       "edge-tiling" = true;
     };
 
+    # Disable the annoying "hot corner" that shows the overview
+    "org/gnome/desktop/interface" = {
+      "enable-hot-corners" = false;
+    };
+
     # Give the full set of titlebar buttons
     "org/gnome/desktop/wm/preferences" = {
       "button-layout" = "appmenu:minimize,maximize,close";
