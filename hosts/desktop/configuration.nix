c1da5d887f451563c843e564b51291f26008fd30
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-24 13:37:13 +0100

    96: Move locale to a module
    Generation  Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    96 current  2024-05-24 13:37:06  23.11.20240522.4639777   6.1.91                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -18,6 +18,7 @@
       ./hardware-configuration.nix
       ../../modules/nixos/firefox.nix
       ../../modules/nixos/gnome.nix
+      ../../modules/nixos/locale.nix
       ../../modules/nixos/nvidia.nix
       ../../modules/nixos/theme.nix
       ../../modules/nixos/thunderbird.nix
@@ -38,24 +39,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/London";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
