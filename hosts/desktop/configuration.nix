f2df63b10cdfff00f6991fd3b89cc5a12bfe0ec5
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-24 21:20:39 +0100

    110: Add java
    Generation   Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    110 current  2024-05-24 21:20:31  23.11.20240522.4639777   6.1.91                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -104,6 +104,9 @@
 
   programs.steam.enable = true;
 
+  # Enable Java, using the default OpenJDK
+  programs.java.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
