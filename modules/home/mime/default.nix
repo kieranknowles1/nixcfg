c3b9898a32e75befb265702914fa66013a2784fa
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-24 21:49:36 +0100

    114: Manage mime types
    Generation   Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    114 current  2024-05-24 21:49:28  23.11.20240522.4639777   6.1.91                          *

---
+++ modules/home/mime/default.nix
@@ -0,0 +1,19 @@
+# MIME definitions and associations
+{ config, lib, pkgs, ... }:
+let
+  # Base directory for MIME definitions. The definitions themselves
+  # are stored in the `packages` subdirectory.
+  mime-directory = "${config.xdg.dataHome}/mime";
+in
+{
+  # Copy definitions into the user's mime directory
+  home.file."${mime-directory}/packages" = {
+    source = ./definitions;
+    recursive = true;
+  };
+
+  # Update the user's mime database when rebuilding
+  home.activation.update-mime-database = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.shared-mime-info}/bin/update-mime-database "${mime-directory}"
+  '';
+}
