4af501369ff98a618c11b05b8329a3a5fe0b3c42
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-24 19:19:42 +0100

    108: Use a function to create the user
    Generation   Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    108 current  2024-05-24 19:19:36  23.11.20240522.4639777   6.1.91                          *

--- flake.nix
+++ flake.nix
@@ -27,11 +27,37 @@
     ...
   }@inputs:
   let
+    # Function to create a user for a host
+    mk-user = {
+      hostName, # Host name # TODO: Make a "users" folder instead and remove home.nix from the hosts
+      userName, # Login name
+      displayName, # Name shown in UIs
+      isSudoer ? false, # Whether the user should be able to sudo
+      shell # Package for the user's shell
+    }: {
+      users.users.${userName} = {
+        # A regular user that can log in
+        isNormalUser = true; # A regular user that can log in
+        # User's full name
+        description = displayName;
+
+        # Give everyone "networkmanager" membership so they can connect to networks
+        # Give sudoers "wheel" membership so they can sudo
+        extraGroups = [ "networkmanager" ]
+          ++ (if isSudoer then [ "wheel" ] else []);
+
+        shell = shell;
+      };
+
+      home-manager.users.${userName} = import ./hosts/${hostName}/home.nix { userName = userName; };
+    };
+
     # Function to create a host configuration
     # Imports ./hosts/$host/configuration.nix
     mk-host = {
-      name,
-      system
+      name, # Host name. Should match the key in nixosConfigurations for rebuilds to detect it automatically
+      system, # System type. Usually x86_64-linux
+      users, # A list of users to create, as returned by mk-user
     }: let
       pkgs = import nixpkgs { system = system; allowUnfree = true; };
       pkgs-unstable = import nixpkgs-unstable { system = system; config.allowUnfree = true; };
@@ -47,34 +73,34 @@
         stylix.nixosModules.stylix
         ./hosts/${name}/configuration.nix
         {
-            # Define a user account. Don't forget to set a password with ‘passwd’.
-            users.users.kieran = {
-              isNormalUser = true;
-              description = "Kieran";
-              extraGroups = [ "networkmanager" "wheel" ];
-
-              # Make Nu our default shell
-              shell = pkgs.nushell;
-            };
-
-            home-manager = {
-              useGlobalPkgs = true;
-              # Pass all flake inputs to home manager configs
-              extraSpecialArgs = { inherit inputs system pkgs-unstable; };
-              backupFileExtension = "backup";
-              users.kieran = import ./hosts/desktop/home.nix { userName = "kieran"; };
-            };
+          # Base home-manager for all users
+          # TODO: Move this to a module
+          home-manager = {
+            useGlobalPkgs = true;
+            # Pass all flake inputs to home manager configs
+            extraSpecialArgs = { inherit inputs system pkgs-unstable; };
+            backupFileExtension = "backup";
+          };
         }
       ];
     };
-  in {
 
+    pkgs-linux64 = import nixpkgs { system = "x86_64-linux"; allowUnfree = true; };
+  in {
     nixosConfigurations = {
       desktop = mk-host {
         name = "desktop";
         system = "x86_64-linux";
+        users = [
+          mk-user {
+            hostName = "desktop";
+            userName = "kieran";
+            displayName = "Kieran";
+            isSudoer = true;
+            shell = pkgs-linux64.nushell;
+          }
+        ];
       };
     };
   };
 }
-
