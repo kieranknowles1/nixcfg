7267cbaf570d28a24b51718776c1b37c05428943
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-27 00:17:16 +0100

    desktop#137: Associate files with resaver
    Generation   Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    137 current  2024-05-27 00:17:09  23.11.20240522.4639777   6.1.91                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2943 -> 2943 (7 paths added, 7 paths removed, delta +0, disk usage +96B).

--- modules/home/games.nix
+++ modules/home/games.nix
@@ -1,19 +1,36 @@
 { pkgs, config, ... }: let
   applicationsDir = "${config.xdg.dataHome}/applications";
 
-  # TODO: Don't hardcode the path to the desktop file
-  resaverDesktopContents = ''
-    [Desktop Entry]
-    Type=Application
-    Version=1.0
-    Name=ReSaver
-    Comment=Skyrim and Fallout 4 savegame editor
-    Path=/home/kieran/Games/modding-tools/resaver/target
-    MimeType=application/x-skyrimsave;
-    Exec=java -jar /home/kieran/Games/modding-tools/resaver/target/ReSaver.jar %u
-  '';
+  resaverDesktop = {
+    fileName = "nixcfg-resaver.desktop";
+    # TODO: Don't hardcode the path to the desktop file
+    # TODO: Use the nix style of getting the java executable
+    contents = ''
+      [Desktop Entry]
+      Type=Application
+      Version=1.0
+      Name=ReSaver
+      Comment=Skyrim and Fallout 4 savegame editor
+      Path=/home/kieran/Games/modding-tools/resaver/target
+      MimeType=application/x-skyrimsave;
+      Exec=java -jar /home/kieran/Games/modding-tools/resaver/target/ReSaver.jar %u
+    '';
+  };
 in {
-  # Install the desktop file to ~/.local/share/applications
   # TODO: Condition this based on the host's configuration
-  home.file."${applicationsDir}/nixcfg-resaver.desktop".text = resaverDesktopContents;
+  config = {
+    # Install the desktop file to ~/.local/share/applications
+    home.file."${applicationsDir}/${resaverDesktop.fileName}".text = resaverDesktop.contents;
+
+    # Associate our mime types with the desktop file. See ./mime/
+    # TODO: Make the link clickable
+    xdg.mimeApps = {
+      enable = true;
+
+      associations.added = {
+        "application/x-skyrimsave" = resaverDesktop.fileName;
+        "application/x-fallout4save" = resaverDesktop.fileName;
+      };
+    };
+  };
 }
