9656a0994d8eb3164a1dd32613063ac8a59db507
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-28 15:41:13 +0100

    desktop#144: Remove unused code
    Generation   Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    144 current  2024-05-28 15:39:16  23.11.20240525.9d29cd2   6.1.91                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2946 -> 2946 (0 paths added, 0 paths removed, delta +0, disk usage +0B).

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -1,15 +1,6 @@
 # Module to install game tools
-{ config, lib, pkgs-unstable, pkgs, ... }: let
-  # TODO: Don't like this hardcoded path, but ReSaver is only hosted on Nexus Mods which I'd have to pass my API key to download from
-  resaverJar = /home/kieran/Games/modding-tools/resaver/target/ReSaver.jar;
-
-  resaverDesktop = pkgs.makeDesktopItem {
-    name = "resaver-desktop";
-    desktopName = "ReSaver";
-    # We use %f to pass the opened file as the first argument
-    exec = "${pkgs.jre8} -jar ${resaverJar} %f";
-  };
-in {
+{ config, lib, pkgs-unstable, pkgs, ... }:
+{
   options = {
     custom.games.enable = lib.mkEnableOption "games";
   };
