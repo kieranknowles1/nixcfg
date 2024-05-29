7ecba477e16aac249f43aeecad06a6ca796f49ad
Kieran <kieranknowles11@hotmail.co.uk>
2024-05-30 00:39:41 +0100

    Explain why disabling checks is safe

--- lib/docs.nix
+++ lib/docs.nix
@@ -63,6 +63,9 @@ in {
         importer
         # Don't eval flake inputs, we don't want to generate documentation for them.
         # The checks this disables are already being done during build time.
+
+        # This works because evalModules is not passed anything from the flake inputs, such as nixpkgs.
+        # Disabling checks supresses the missing input error (which is expected and any unexpected error will have already been caught).
         { config._module.check = false; }
       ];
     };
