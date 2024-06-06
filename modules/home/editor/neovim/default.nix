e4642408848003bbbd72a03aa94c7da1fce981bf
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-06 15:21:46 +0100

    204: Add language servers
    Generation   Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    204 current  2024-06-06 15:21:38  24.05.20240605.0b8e7a1   6.6.32                          *
    
    <<< /run/current-system
    >>> result
    Added packages:
    [A.]  #1  vimplugin-nvim-lspconfig  2024-05-16
    Closure size: 2115 -> 2116 (14 paths added, 13 paths removed, delta +1, disk usage +994.7KiB).

--- modules/home/editor/neovim/default.nix
+++ modules/home/editor/neovim/default.nix
@@ -1,12 +1,17 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   # TODO: Configure neovim
   config = lib.mkIf (config.custom.development.editor == "neovim") {
     programs.neovim = {
       enable = true;
+
+      plugins = with pkgs.vimPlugins; [
+        nvim-lspconfig
+      ];
     };
   };
 }
