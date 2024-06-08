38b941d7e99c6bc5310ab5bd98048bc36052bae4
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-08 21:42:34 +0100

    234: Add shebang automatically
    Generation   Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    234 current  2024-06-08 21:42:23  24.05.20240606.9b5328b   6.6.32                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2083 -> 2083 (21 paths added, 21 paths removed, delta +0, disk usage +24B).

--- packages/default.nix
+++ packages/default.nix
@@ -1,15 +1,34 @@
 {
   pkgs
-}: {
-  clean-skse-cosaves = pkgs.stdenv.mkDerivation rec {
-    pname = "clean-skse-cosaves";
-    version = "1.0.0";
-    src = ./clean-skse-cosaves.py;
+}: let
+  /**
+    Package a Python script as a standalone executable.
 
-    dontUnpack = true;
+    A shebang for Python 3 is prepended automatically, although including it in
+    the source is recommended to aid in debugging.
+
+    # Arguments
+    script :: String : The name of the source file and the output executable.
+
+    version :: String : The version of the script.
+   */
+  packagePythonScript = script: version: pkgs.stdenv.mkDerivation rec {
+    pname = "clean-skse-cosaves";
+    inherit version;
+    src = ./${pname}.py;
+
+    dontUnpack = true; # This is a text file, unpacking is only applicable to archives
     installPhase = ''
       mkdir -p $out/bin
-      cp $src $out/bin/${pname}
+
+      src_code=$(cat $src)
+      shebang="#!${pkgs.python3}/bin/python3"
+
+      echo "$shebang" > $out/bin/${pname}
+      echo "$src_code" >> $out/bin/${pname}
+      chmod +x $out/bin/${pname}
     '';
   };
+in {
+  clean-skse-cosaves = packagePythonScript "clean-skse-cosaves" "1.0.0";
 }
