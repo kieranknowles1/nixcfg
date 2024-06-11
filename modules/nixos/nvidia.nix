9e3ba99aaec0c63716934321dc70980332faab8d
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-11 14:20:31 +0100

    Explain that wayland is experimental

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -18,6 +18,9 @@
       modesetting.enable = true;
       powerManagement.enable = false;
       powerManagement.finegrained = false;
+      # The open source drivers are not as good as the proprietary ones and don't seem to work with Wayland.
+      # NOTE: Even though I'm using Wayland, I still consider it experimental with Nvidia. Intel integrated works
+      # just fine though and I'd like to have the same on all my machines.
       open = false;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
