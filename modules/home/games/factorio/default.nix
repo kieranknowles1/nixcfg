9720cd1a9fe6927abe85cc27f7eca5f08a8597a6
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-16 22:22:10 +0100

    Update flake inputs.
    <<< /run/current-system
    >>> result
    Version changes:
    [U*]  #01  home-manager                          0-unstable-2024-06-04 -> 0-unstable-2024-06-13
    [U.]  #02  initrd-linux                          6.6.33 -> 6.6.34
    [U.]  #03  linux                                 6.6.33, 6.6.33-modules, 6.6.33-modules-shrunk -> 6.6.34, 6.6.34-modules, 6.6.34-modules-shrunk
    [U.]  #04  nixos-system-desktop                  24.11.20240614.7894cf0 -> 24.11.20240616.9f0e39e
    [U*]  #05  nvidia-x11                            550.90.07-6.6.33, 550.90.07-6.6.33-bin, 550.90.07-6.6.33-lib32 -> 550.90.07-6.6.34, 550.90.07-6.6.34-bin, 550.90.07-6.6.34-lib32
    [U.]  #06  osinfo-db                             20240510 -> 20240523
    [U*]  #07  thunderbird                           115.11.1 -> 115.12.0
    [U.]  #08  thunderbird-unwrapped                 115.11.1 -> 115.12.0
    [C.]  #09  udev-rules                            <none> -> <none> x2
    [U.]  #10  vscode                                1.90.0 -> 1.90.1
    [U.]  #11  vscode-extension-github-copilot       1.202.0 -> 1.203.0
    [U.]  #12  vscode-extension-github-copilot-chat  0.17.2024061301 -> 0.17.2024061401
    [U.]  #13  vscode-extension-ms-python-python     2024.9.11641010 -> 2024.9.11661012
    Added packages:
    [A.]  #1  extra-utils                   <none>
    [A.]  #2  initrd-fsinfo                 <none>
    [A.]  #3  initrd-kmod-blacklist-ubuntu  <none>
    [A.]  #4  link-units                    <none>
    [A.]  #5  stage                         1-init.sh
    Closure size: 2076 -> 2082 (118 paths added, 112 paths removed, delta +6, disk usage +23.8MiB).

--- modules/home/games/factorio/default.nix
+++ modules/home/games/factorio/default.nix
@@ -15,6 +15,8 @@
     '';
   in "${result}/blueprints.txt";
 in {
+  # There's currently no way to create a blueprint binary directly, so we provide a string
+  # that can be imported into Factorio.
   config = lib.mkIf hostConfig.custom.games.enable {
     home.file."Games/factorio-blueprints.txt".source = blueprintString;
   };
