2e47aaefadd9d1b090cda9e1ff1360df76fcedfb
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-17 22:36:10 +0100

    Set flake session variable
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2094 -> 2094 (9 paths added, 9 paths removed, delta +0, disk usage +40B).

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -1,12 +1,20 @@
 # Options that are used in multiple modules
 {
   lib,
+  config,
   ...
 }: {
   options = {
     custom.repoPath = lib.mkOption {
       description = "Path to the repository on disk, relative to the home directory";
       type = with lib.types; uniq str;
+    };
+  };
+
+  config = {
+    systemd.user.sessionVariables = {
+      # NixHelper uses this to find the repository
+      FLAKE = "${config.home.homeDirectory}/${config.custom.repoPath}";
     };
   };
 }
