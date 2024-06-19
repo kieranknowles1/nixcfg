b2fe080b1935c9f13005204c0119f14901620c09
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-19 14:25:23 +0100

    Autostart sxhkd
    383#desktop: Autostart sxhkd
    
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    383 current  2024-06-19 14:25:21  24.11.20240617.8aaa130  6.6.34                          *
    
    <<< /run/current-system
    >>> result
    Added packages:
    [A.]  #1  hm_homekieran.configautostartsxhkd.desktop  <none>
    Closure size: 2143 -> 2144 (9 paths added, 8 paths removed, delta +1, disk usage +672B).

--- modules/home/shortcuts.nix
+++ modules/home/shortcuts.nix
@@ -1,6 +1,20 @@
-# Keyboard shortcuts managed by AutoKey
-{ pkgs, config, ... }:
-{
+# Keyboard shortcuts managed by sxhkd
+{ pkgs, config, ... }: let
+  # TODO: Move this to the lib and use it for ReSaver
+  /**
+    Generate an XDG desktop entry file for a command.
+    See https://wiki.archlinux.org/title/desktop_entries#Application_entry
+    for more information.
+   */
+  mkDesktopEntry = { name, description, command }: ''
+    [Desktop Entry]
+    Type=Application
+    Version=1.0
+    Name=${name}
+    Comment=${description}
+    Exec=${command}
+  '';
+in {
   services.sxhkd = {
     enable = true;
 
@@ -9,5 +23,12 @@
       "ctrl + alt + e" = "fsearch"; # Open FSearch (Everything clone)
       "ctrl + alt + Escape" = "gnome-system-monitor"; # Open task manager. Copy of Windows shortcut
     };
+  };
+
+  # Autostart sxhkd
+  home.file."${config.xdg.configHome}/autostart/sxhkd.desktop".text = mkDesktopEntry {
+    name = "sxhkd";
+    description = "Simple X Hotkey Daemon";
+    command = "sxhkd";
   };
 }
