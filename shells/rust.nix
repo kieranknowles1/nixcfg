73544fc057f9e95ad65f21ef68ebc217a25e7323
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-22 00:44:09 +0100

    Get rust-analyzer working

--- shells/rust.nix
+++ shells/rust.nix
@@ -6,5 +6,10 @@
     cargo
     rustc
     gcc # Rust needs a linker
+    pkg-config # Needed for rust-analyzer
   ];
+
+  # Rust-analyzer requires the standard library's source code to give
+  # completions.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
