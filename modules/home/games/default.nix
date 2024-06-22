69911e1843100cb7bbe787e3773af011731db971
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-23 00:41:54 +0100

    413#desktop: Use mkDestopEntry for resaver
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    413 current  2024-06-23 00:41:50  24.11.20240622.8b75046  6.6.35                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2422 -> 2422 (12 paths added, 12 paths removed, delta +0, disk usage -80B).

--- modules/home/games/default.nix
+++ modules/home/games/default.nix
@@ -1,20 +1,7 @@
-{ pkgs, lib, config, hostConfig, ... }: let
+{ pkgs, lib, config, hostConfig, flake, ... }: let
   applicationsDir = "${config.xdg.dataHome}/applications";
 
-  resaverDesktop = {
-    fileName = "nixcfg-resaver.desktop";
-    # TODO: Don't hardcode the path to the desktop file
-    contents = ''
-      [Desktop Entry]
-      Type=Application
-      Version=1.0
-      Name=ReSaver
-      Comment=Skyrim and Fallout 4 savegame editor
-      Path=/home/kieran/Games/modding-tools/resaver/target
-      MimeType=application/x-skyrimsave;
-      Exec=${pkgs.jdk21}/bin/java -jar /home/kieran/Games/modding-tools/resaver/target/ReSaver.jar %u
-    '';
-  };
+  resaverDesktopFileName = "nixcfg-resaver.desktop";
 in {
   imports = [
     ./factorio
@@ -22,15 +9,21 @@ in {
 
   config = lib.mkIf hostConfig.custom.games.enable {
     # Install the desktop file to ~/.local/share/applications
-    home.file."${applicationsDir}/${resaverDesktop.fileName}".text = resaverDesktop.contents;
+    home.file."${applicationsDir}/${resaverDesktopFileName}".text = flake.lib.package.mkDesktopEntry {
+      name = "ReSaver";
+      description = "Skyrim and Fallout 4 savegame editor";
+      # TODO: Don't hardcode the path to the executable and working directory
+      command = "${pkgs.jdk21}/bin/java -jar /home/kieran/Games/modding-tools/resaver/target/ReSaver.jar %u";
+      workingDirectory = "/home/kieran/Games/modding-tools/resaver/target";
+    };
 
-    # Associate our mime types with the desktop file. See [[./mime/default.nix]]
+    # Associate our mime types with the desktop file. See [[../mime/default.nix]]
     xdg.mimeApps = {
       enable = true;
 
       associations.added = {
-        "application/x-skyrimsave" = resaverDesktop.fileName;
-        "application/x-fallout4save" = resaverDesktop.fileName;
+        "application/x-skyrimsave" = resaverDesktopFileName;
+        "application/x-fallout4save" = resaverDesktopFileName;
       };
     };
 
