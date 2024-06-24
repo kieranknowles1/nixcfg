91e2ebd2c1361eea06fe8fbf257414a680a9609a
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-24 01:11:03 +0100

    416#desktop: Generate docs index
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    416 current  2024-06-24 01:10:59  24.11.20240622.8b75046  6.6.35                          *
    
    <<< /run/current-system
    >>> result
    Added packages:
    [A.]  #1  hm_Documentssrcnixcfgdocsreadme.md  <none>
    Removed packages:
    [R.]  #1  hm_readme.md  <none>
    Closure size: 2744 -> 2744 (9 paths added, 9 paths removed, delta +0, disk usage -128B).

--- modules/home/docs.nix
+++ modules/home/docs.nix
@@ -1,10 +1,63 @@
 # Module to include generated documentation for the flake and its options
-{ flake, config, ... }: let
-  repoPath = config.custom.repoPath;
+{ flake, lib, config, ... }: let
+  docsPath = "${config.custom.repoPath}/docs";
+
+  /**
+   * Generate an index of the documentation files.
+   */
+  mkIndex = files: let
+    # Get an alphabetically sorted list of the files
+    fileNames = builtins.attrNames files;
+
+    links = lib.lists.forEach fileNames (name: "- [${name}](./${name})");
+
+  in ''
+    # Documentation index
+
+    This file is the index for all generated documentation files.
+
+    ## Files
+    ${lib.strings.concatStringsSep "\n" links}
+  '';
 in {
-  config = {
-    home.file."${repoPath}/docs/lib.md".source = flake.lib.docs.mkFunctionDocs ../../lib;
-    home.file."${repoPath}/docs/host-options.md".source = flake.lib.docs.mkOptionDocs ../nixos/default.nix;
-    home.file."${repoPath}/docs/user-options.md".source = flake.lib.docs.mkOptionDocs ./default.nix;
+  options.custom.docs-generate = {
+    file = lib.mkOption {
+      description = ''
+        A file include in the generated documentation.
+
+        The key is the name of the file in the output, and the value is a file containing the content.
+        This file will probably be pointing to a derivation that generates the content.
+
+        All documentation files are generated in the `docs` directory of the flake repository.
+      '';
+
+      example = {
+        "file-a.md" = ./docs-generated/file-a.md;
+      };
+
+      type = lib.types.attrsOf lib.types.path;
+    };
+  };
+
+  config = let
+    docFiles = config.custom.docs-generate.file;
+
+    # Map the files to the correct paths. Having all docs in one option simplifies any changes I might want to make.
+    # No sane language would allow an apostrophe in a variable name, but Nix is not a sane language.
+    # mapAttrs' returns a set where we can change the key and value.
+    homeFiles = lib.attrsets.mapAttrs' (name: value: {
+      name = "${docsPath}/${name}";
+      value = { source = value; };
+    }) docFiles;
+  in {
+    custom.docs-generate.file = {
+      "lib.md" = flake.lib.docs.mkFunctionDocs ../../lib;
+      "host-options.md" = flake.lib.docs.mkOptionDocs ../nixos/default.nix;
+      "user-options.md" = flake.lib.docs.mkOptionDocs ./default.nix;
+    };
+
+    home.file = homeFiles // {
+      "${docsPath}/readme.md".text = mkIndex docFiles;
+    };
   };
 }
