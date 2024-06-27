2a434eaf87b9c91e0eec00d8d7670c2bd5d666e8
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-28 00:52:42 +0100

    423#desktop: Make font configurable
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    423 current  2024-06-28 00:52:37  24.11.20240626.909f744  6.6.35                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2711 -> 2711 (9 paths added, 9 paths removed, delta +0, disk usage +408B).

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -4,10 +4,18 @@
   lib,
   ...
 }: {
-  options = {
-    custom.repoPath = lib.mkOption {
+  options.custom = {
+    repoPath = lib.mkOption {
       description = "Path to the repository on disk, relative to the home directory";
       type = with lib.types; uniq str;
+    };
+
+    fonts = {
+      defaultMono = lib.mkOption {
+        description = "Default monospace font";
+        type = with lib.types; uniq str;
+        default = "DejaVuSansMono";
+      };
     };
   };
 
@@ -15,6 +23,10 @@
     # Set the FLAKE environment variable for use in nixhelper and other scripts
     systemd.user.sessionVariables = {
       FLAKE = "${config.home.homeDirectory}/${config.custom.repoPath}";
+    };
+
+    fonts.fontconfig.defaultFonts = {
+      monospace = [config.custom.fonts.defaultMono];
     };
   };
 }
