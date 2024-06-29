17525ac4e6b8ace28786d156c45cf358aafc5942
Kieran <kieranknowles11@hotmail.co.uk>
2024-06-29 17:26:29 +0100

    429#desktop: Generate keybinding docs
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    429 current  2024-06-29 17:27:05  24.11.20240626.909f744  6.6.35                          *
    
    <<< /run/current-system
    >>> result
    Added packages:
    [A.]  #1  shortcuts.md  <none>
    Closure size: 2711 -> 2712 (11 paths added, 10 paths removed, delta +1, disk usage +11.4KiB).

--- modules/home/shortcuts.nix
+++ modules/home/shortcuts.nix
@@ -1,28 +1,87 @@
 # Keyboard shortcuts managed by sxhkd
 {
   config,
+  lib,
   flake,
   pkgs,
   ...
 }: let
   sxhkd = pkgs.sxhkd;
-in {
-  services.sxhkd = {
-    enable = true;
-    package = sxhkd;
 
-    keybindings = {
-      "alt + t" = "kgx"; # Open terminal
-      "ctrl + alt + e" = "fsearch"; # Open FSearch (Everything clone)
-      "ctrl + shift + Escape" = "resources"; # Open task manager. Copy of Windows shortcut
+  mkDocs = bindings: let
+    keys = builtins.attrNames bindings;
+
+    bindingList = lib.lists.forEach keys (key: let
+      description = bindings.${key}.description;
+    in "- `${key}` - ${description}");
+  in ''
+    # Keyboard shortcuts
+
+    The following keyboard shortcuts are available globally:
+
+    ${lib.strings.concatStringsSep "\n" bindingList}
+  '';
+in {
+  options.custom = {
+    shortcuts = lib.mkOption {
+      description = ''
+        A set of keyboard shortcuts to be managed by sxhkd.
+        The key in the set is the binding, and the value contains the action to be executed and
+        a description for documentation purposes.
+      '';
+
+      type = lib.types.attrsOf (lib.types.submodule {
+        options = {
+          action = lib.mkOption {
+            type = lib.types.str;
+            description = "The action to be executed when the keybinding is pressed";
+          };
+          description = lib.mkOption {
+            type = lib.types.str;
+            description = "Brief, one-line description of the keybinding";
+          };
+        };
+      });
     };
   };
 
-  # Autostart sxhkd
-  home.file."${config.xdg.configHome}/autostart/sxhkd.desktop".text = flake.lib.package.mkDesktopEntry {
-    name = "sxhkd";
-    description = "Simple X Hotkey Daemon";
-    command = "sxhkd";
-    version = sxhkd.version;
+  config = {
+    # Default shortcuts
+    custom.shortcuts = {
+      "alt + t" = {
+        action = "kgx";
+        description = "Open terminal";
+      };
+      "ctrl + alt + e" = {
+        action = "fsearch";
+        description = "Open FSearch (Everything clone)";
+      };
+      "ctrl + shift + Escape" = {
+        action = "resources";
+        description = "Open task manager.";
+      };
+    };
+
+    # Generate documentation
+    custom.docs-generate.file."shortcuts.md" = {
+      description = "Keyboard shortcuts";
+      source = builtins.toFile "shortcuts.md" (mkDocs config.custom.shortcuts);
+    };
+
+    # Apply options
+    services.sxhkd = {
+      enable = true;
+      package = sxhkd;
+
+      keybindings = builtins.mapAttrs (name: value: value.action) config.custom.shortcuts;
+    };
+
+    # Autostart sxhkd
+    home.file."${config.xdg.configHome}/autostart/sxhkd.desktop".text = flake.lib.package.mkDesktopEntry {
+      name = "sxhkd";
+      description = "Simple X Hotkey Daemon";
+      command = "sxhkd";
+      version = sxhkd.version;
+    };
   };
 }
