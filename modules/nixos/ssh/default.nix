08e0a788d7c617e8876cb581f5f76d7422a12e9f
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-03 15:42:57 +0100

    445#desktop: Add known hosts
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    445 current  2024-07-03 15:43:36  24.11.20240702.516f9ac  6.6.36                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2721 -> 2721 (9 paths added, 9 paths removed, delta +0, disk usage +184B).

--- modules/nixos/ssh/default.nix
+++ modules/nixos/ssh/default.nix
@@ -8,8 +8,14 @@
       # Only allow public key authentication
       settings.PasswordAuthentication = false;
 
-      # Install signatures of all my hosts
-      # knownHosts =
+      # Install signatures of all my hosts, read from [[./hosts]]
+      # File name is the hostname, content is the signature returned by `ssh-keyscan ${hostname}`
+      # this will work for localhost and remote hosts
+      # Multiple algorithms will be listed, prefer "ssh-ed25519" as it is the most secure
+      knownHosts = builtins.mapAttrs (name: value: {
+        hostNames = [name];
+        publicKeyFile = ./hosts/${name};
+      }) (builtins.readDir ./hosts);
     };
 
     # Accept any of my public keys, read from [[./keys]]
