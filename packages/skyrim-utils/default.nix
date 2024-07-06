9c5da98b4431b98e54b06c76e486a5a70d13ee14
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-07 00:04:36 +0100

    452#desktop: Add open latest save option
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    452 current  2024-07-07 00:05:34  24.11.20240702.516f9ac  6.6.36                          *
    
    <<< /run/current-system
    >>> result
    Version changes:
    [U*]  #1  skyrim-utils  1.0.0 -> 1.1.0
    Closure size: 2721 -> 2721 (21 paths added, 21 paths removed, delta +0, disk usage +719.7KiB).

--- packages/skyrim-utils/default.nix
+++ packages/skyrim-utils/default.nix
@@ -1,8 +1,8 @@
 {pkgs}:
 pkgs.rustPlatform.buildRustPackage {
   pname = "skyrim-utils";
-  version = "1.0.0";
+  version = "1.1.0";
   src = ./.;
 
-  cargoHash = "sha256-vBHdqKTWxPnvRL7jKwvjBS/ccHDikT7hqWdRF0tGwXI=";
+  cargoHash = "sha256-pcwaj+KhvhIS4aWh5C2KH23KcqKcxeVv/8Xcb2wecZQ=";
 }
