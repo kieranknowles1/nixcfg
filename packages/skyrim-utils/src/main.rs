b7d81b03be8bf6a74d993b357bc8fe42e1653bdb
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-07 00:04:36 +0100

    452#desktop: Add open latest save option
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    452 current  2024-07-07 00:05:34  24.11.20240702.516f9ac  6.6.36                          *
    
    <<< /run/current-system
    >>> result
    Version changes:
    [U*]  #1  skyrim-utils  1.0.0 -> 1.1.0
    Closure size: 2721 -> 2721 (21 paths added, 21 paths removed, delta +0, disk usage +719.7KiB).

--- packages/skyrim-utils/src/main.rs
+++ packages/skyrim-utils/src/main.rs
@@ -1,9 +1,10 @@
 // cSpell: words skse shellexpand skyrim
 
-use std::fs;
+use std::fs::{self, DirEntry};
 use std::io::Result;
 use std::path::PathBuf;
 use std::collections::HashSet;
+use clap::builder::OsStr;
 use shellexpand;
 use clap::{Args, Parser};
 
@@ -11,6 +12,8 @@ use clap::{Args, Parser};
 enum Arguments {
     /// Clean up orphaned .skse files
     Clean(CleanArgs),
+    /// Open the latest save file
+    Latest(LatestArgs),
 }
 
 #[derive(Args)]
@@ -34,10 +37,33 @@ impl CleanArgs {
     }
 }
 
+#[derive(Args)]
+struct LatestArgs {}
+
+impl LatestArgs {
+    fn run(&self, save_dir: &str) -> Result<()> {
+        let latest = SaveFiles::get_latest(&save_dir)?;
+
+        open::that(latest)?;
+
+        Ok(())
+    }
+}
+
 /// A representation of all found .ess and .skse files in a directory
 struct SaveFiles {
     ess_files: HashSet<PathBuf>,
     skse_files: HashSet<PathBuf>,
+}
+
+/// Get the extension of a file as a plain string rather than an OsStr
+/// Returns None if the file has no extension or if the extension is not valid UTF-8
+/// (which should never happen, but Linux allows newlines so who knows what else is possible)
+fn extension_plain_str(path: &PathBuf) -> Option<&str> {
+    match path.extension() {
+        Some(ext) => ext.to_str(),
+        None => None,
+    }
 }
 
 impl SaveFiles {
@@ -48,12 +74,7 @@ impl SaveFiles {
         for file in fs::read_dir(dir)? {
             let path = file?.path();
 
-            let extension = match path.extension() {
-                Some(ext) => ext.to_str(),
-                None => None,
-            };
-
-            match extension {
+            match extension_plain_str(&path) {
                 Some("ess") => ess_files.insert(path),
                 Some("skse") => skse_files.insert(path),
                 _ => continue, // It is valid to have files in the directory that are not .ess or .skse files
@@ -64,6 +85,25 @@ impl SaveFiles {
             ess_files,
             skse_files,
         })
+    }
+
+    fn get_latest(dir: &str) -> Result<PathBuf> {
+        fn get_modified_time(file: &DirEntry) -> Option<std::time::SystemTime> {
+            match file.metadata() {
+                Ok(metadata) => metadata.modified().ok(),
+                Err(_) => None,
+            }
+        }
+
+        let newest = fs::read_dir(dir)?
+            .flatten() // Remove any errors
+            .filter(|file| extension_plain_str(&file.path()) == Some("ess"))
+            .max_by_key(get_modified_time);
+
+        match newest {
+            Some(file) => Ok(file.path()),
+            None => Err(std::io::Error::new(std::io::ErrorKind::NotFound, "No files found")),
+        }
     }
 
     /// Get all .skse files that do not have a corresponding .ess file
@@ -82,6 +122,7 @@ fn main() -> Result<()> {
 
     match args {
         Arguments::Clean(clean_args) => clean_args.run(&save_dir),
+        Arguments::Latest(latest_args) => latest_args.run(&save_dir),
     }?;
 
     Ok(())
