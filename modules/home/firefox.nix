87151ca58d3ab8c231563286ba5e021cedf34ff6
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-07 17:34:24 +0100

    454#desktop: Manage firefox in home manager exclusively
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    454 current  2024-07-07 17:35:10  24.11.20240702.516f9ac  6.6.36                          *
    
    <<< /run/current-system
    >>> result
    Version changes:
    [C-]  #1  firefox  127.0.2 x2 -> 127.0.2
    Removed packages:
    [R.]  #1  firefox-policies.json  <none>
    Closure size: 2721 -> 2719 (23 paths added, 25 paths removed, delta -2, disk usage -1.8MiB).

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,14 +1,28 @@
 # Per-user Firefox settings
 # See also: [[../nixos/firefox.nix]]
 {
+  hostConfig,
   inputs,
   system,
   ...
 }: let
+  isDesktop = hostConfig.custom.deviceType == "desktop";
+
   pkgs-stable = inputs.nixpkgs.legacyPackages.${system};
 in {
   programs.firefox = {
-    enable = true;
+    enable = isDesktop;
+
+    policies = {
+      AutofillCreditCardEnabled = false;
+      # Updates are managed by Nix
+      DisableAppUpdate = true;
+      DisableTelemetry = true;
+      # Extensions are also managed
+      ExtensionUpdate = false;
+      # I use Bitwarden for this
+      PasswordManagerEnabled = false;
+    };
 
     # Firefix updates frequently and takes a long time to build, so we use the
     # stable channel here.
@@ -35,7 +49,7 @@ in {
 
   # Open PDFs in Firefox
   xdg.mimeApps = {
-    enable = true;
+    enable = isDesktop;
 
     associations.added = {
       "application/pdf" = "firefox.desktop";
