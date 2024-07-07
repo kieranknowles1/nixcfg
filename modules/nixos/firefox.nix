85b7f61da391553327cd813c8dc31ac201f12e11
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-07 17:34:24 +0100

    454#desktop: Manage firefox in home manager exclusively
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    454 current  2024-07-07 17:35:10  24.11.20240702.516f9ac  6.6.36                          *
    
    <<< /run/current-system
    >>> result
    Version changes:
    [C-]  #1  firefox  127.0.2 x2 -> 127.0.2
    Removed packages:
    [R.]  #1  firefox-policies.json  <none>
    Closure size: 2721 -> 2719 (23 paths added, 25 paths removed, delta -2, disk usage -1.8MiB).

--- modules/nixos/firefox.nix
+++ modules/nixos/firefox.nix
@@ -1,35 +1,13 @@
 # System level Firefox settings
 # See also: [[../home/firefox.nix]]
 {
-  inputs,
-  system,
   pkgs,
   ...
-}: let
-  pkgs-stable = inputs.nixpkgs.legacyPackages.${system};
-in {
+}: {
   environment.gnome.excludePackages = with pkgs; [
     # GNOME's built-in browser
     epiphany
     # GNOME's document viewer. Firefox does a better job at this
     evince
   ];
-
-  # Install firefox. Extensions are managed by home manager
-  programs.firefox = {
-    enable = true;
-
-    package = pkgs-stable.firefox;
-
-    policies = {
-      AutofillCreditCardEnabled = false;
-      # Updates are managed by Nix
-      DisableAppUpdate = true;
-      DisableTelemetry = true;
-      # Extensions are also managed
-      ExtensionUpdate = false;
-      # I use Bitwarden for this
-      PasswordManagerEnabled = false;
-    };
-  };
 }
