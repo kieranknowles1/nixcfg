5435add547feabfae0b8d30cd6f02cc4961dc100
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-08 12:06:52 +0000

    61#laptop: Disable networkmanager plugins
    Generation  Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    61 current  2024-07-08 12:09:34  24.11.20240702.516f9ac  6.6.36                          *
    
    <<< /run/current-system
    >>> result
    Removed packages:
    [R-]  #01  NetworkManager-fortisslvpn-gnome  1.4.0
    [R-]  #02  NetworkManager-iodine-gnome       1.2.0-unstable-2024-05-12
    [R-]  #03  NetworkManager-l2tp-gnome         1.20.16
    [R-]  #04  NetworkManager-openconnect        1.2.10
    [R-]  #05  NetworkManager-openvpn            1.12.0
    [R-]  #06  NetworkManager-sstp-gnome         1.3.2
    [R-]  #07  NetworkManager-vpnc               1.2.8
    [R.]  #08  iodine                            0.8.0
    [R.]  #09  openfortivpn                      1.22.1
    [R.]  #10  openvpn                           2.6.11
    [R.]  #11  sstp-client-unstable              2023-03-25
    [R.]  #12  strongswan                        5.9.14
    [R.]  #13  vpnc-unstable                     2021-11-04
    [R.]  #14  xl2tpd                            1.3.18
    Closure size: 2196 -> 2182 (28 paths added, 42 paths removed, delta -14, disk usage -15.4MiB).

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,10 +1,21 @@
-{...}: {
+{pkgs, lib, ...}: {
   config = {
     # Enable networking
     networking = {
       networkmanager.enable = true;
       # This is handled by NetworkManager
       wireless.enable = false;
+
+      # Disable plugins we don't use
+      networkmanager.plugins = lib.mkForce [
+        # networkmanager-fortisslvpn # VPN plugin for Fortinet
+        # networkmanager-iodine # Tunnel to get through firewalls
+        # networkmanager-l2tp # Another VPN plugin
+        # networkmanager-openconnect # Cisco VPN plugin
+        # networkmanager-openvpn # Another VPN plugin
+        # networkmanager-vpnc # Another VPN plugin
+        # networkmanager-sstp # What do you know, another VPN
+      ];
     };
 
     # Enable resolving *.local hostnames via mDNS
