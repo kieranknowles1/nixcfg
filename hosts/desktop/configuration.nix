8f435bd147e9e1019a068bd37538fd89a50f7050
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-08 17:10:16 +0100

    Fix desktop build and format code

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,10 +1,14 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   # Enable everything needed for this configuration
   config.custom = {
-    user.kieran = import ../../users/kieran.nix pkgs;
+    user.kieran = import ../../users/kieran.nix {inherit pkgs config;};
 
     deviceType = "desktop";
     repoPath = "/home/kieran/Documents/src/nixcfg";
