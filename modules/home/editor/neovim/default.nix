6316d44148344721a549593968d4c696c12cbe85
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-09 22:50:41 +0100

    Link nvim to edit-config

--- modules/home/editor/neovim/default.nix
+++ modules/home/editor/neovim/default.nix
@@ -5,16 +5,33 @@
   ...
 }: {
   # TODO: Maybe allow multiple editors to be installed at the same time
+  # TODO: Configure all the language servers I use
+  # - Rust
+  # - Nix
+  # - Python
+  # - Yaml
+  # - Nu
+  # - Toml
   config = lib.mkIf (config.custom.editor == "neovim") {
     programs.neovim = {
       enable = true;
+
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
     };
 
-    # Don't manage anything with Nix for this PR
-    # TODO: Manage configs/plugins with Nix
+    # Don't manage anything with Nix for now
+    # TODO: Manage configs/plugins with Nix?
     xdg.configFile."nvim" = {
       source = ./config;
       recursive = true;
+    };
+
+
+    custom.edit-config.programs.nvim = {
+      system-path = "~/.config/nvim";
+      repo-path = "modules/home/editor/neovim/config";
     };
   };
 }
