c55c15cabff4385944a9ececf6a8e13710ec3906
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-09 20:58:30 +0100

    Provision nvim config

--- modules/home/editor/neovim/default.nix
+++ modules/home/editor/neovim/default.nix
@@ -1,9 +1,20 @@
-{pkgs, config, lib, ...}:
 {
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
   # TODO: Maybe allow multiple editors to be installed at the same time
-  config = lib.mkIf config.custom.editor == "neovim" {
-    home.packages = with pkgs; [
-      neovim
-    ];
-  }
+  config = lib.mkIf (config.custom.editor == "neovim") {
+    programs.neovim = {
+      enable = true;
+    };
+
+    # Don't manage anything with Nix for this PR
+    # TODO: Manage configs/plugins with Nix
+    xdg.configFile."nvim" = {
+      source = ./config;
+      recursive = true;
+    };
+  };
 }
