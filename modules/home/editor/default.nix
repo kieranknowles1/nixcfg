6889faf43178727a8cf82c760669ae533017d465
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-10 23:09:15 +0100

    Some k2se malls

--- modules/home/editor/default.nix
+++ modules/home/editor/default.nix
@@ -1,4 +1,8 @@
 {...}: {
+  # TODO: Try out Helix
+  # https://helix-editor.com/
+  # It's built in Rust, evalangism for which is demanded by our crustacean overlords.
+  # I guess it also supports tree-sitter, which is cool.
   imports = [
     ./vscode
     ./neovim
