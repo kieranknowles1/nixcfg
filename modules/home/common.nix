8fbda926a128fcbdef0e4425f2f708fc25d97fd2
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-12 23:59:47 +0100

    514#desktop: Remove home-manager repo path option
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    514 current  2024-07-13 00:00:24  24.11.20240710.97c98e8  6.6.37                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2761 -> 2761 (12 paths added, 12 paths removed, delta +0, disk usage -376B).

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -5,12 +5,6 @@
   ...
 }: {
   options.custom = {
-    # TODO: Remove this in favour of the host's configuration
-    repoPath = lib.mkOption {
-      description = "Path to the repository on disk, relative to the home directory";
-      type = lib.types.str;
-    };
-
     fonts = {
       defaultMono = lib.mkOption {
         description = "Default monospace font";
