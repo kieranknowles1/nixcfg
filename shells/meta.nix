a8f47bb8d3fa07918fcd66580be1feca57582e3c
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-15 16:07:31 +0100

    520#desktop: Remove development.*.enable options
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    520 current  2024-07-15 16:08:32  24.11.20240714.6b8b905  6.6.39                          *
    
    <<< /run/current-system
    >>> result
    Removed packages:
    [R-]  #1  nil  2023-08-09
    Closure size: 2769 -> 2768 (28 paths added, 29 paths removed, delta -1, disk usage -7.7MiB).

--- shells/meta.nix
+++ shells/meta.nix
@@ -1,14 +1,16 @@
 {
   flakeLib,
+  pkgs,
   flakePkgs,
 }:
 flakeLib.shell.mkShellEx {
   name = "meta";
 
-  packages = with flakePkgs; [
-    export-blueprints
-    factorio-blueprint-decoder
-    rebuild
+  packages = with pkgs; [
+    flakePkgs.export-blueprints
+    flakePkgs.factorio-blueprint-decoder
+    flakePkgs.rebuild
+    nil
   ];
 
   shellHook = ''
