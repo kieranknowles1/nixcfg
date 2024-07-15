8b1e1a0ae0fe10a41c79fd3ad12a7f821db09b81
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-15 17:06:02 +0100

    Configure nvim

--- packages/nixvim/default.nix
+++ packages/nixvim/default.nix
@@ -12,12 +12,52 @@ inputs.nixvim.legacyPackages.${pkgs.system}.makeNixvimWithModule {
 
       # Use two spaces for tabs
       shiftwidth = 2;
+
+      # Insert spaces on new lines
+      expandtab = true;
     };
 
     colorschemes.gruvbox.enable = true;
 
     plugins = {
+      treesitter.enable = true;
       lualine.enable = true;
+
+      # Bracket pair color/highlight
+      rainbow-delimiters.enable = true;
+
+      # File browser
+      oil.enable = true;
+
+      # Show untracked changes
+      gitsigns.enable = true;
+
+      # Language servers
+      lsp = {
+        enable = true;
+        servers = {
+          # Nix
+          nil-ls.enable = true;
+          # Lua
+          lua-ls.enable = true;
+        };
+      };
+
+      # Completions
+      cmp = {
+        enable = true;
+        settings = {
+          autoEnableSources = true;
+
+          experimental = {
+            ghost_text = true;
+          };
+
+          sources = [
+            {name = "nvim_lsp";}
+          ];
+        };
+      };
     };
   };
 }
