85f88e61c3618c13f4d8f966dc6ce4f7fcb064c3
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-23 00:05:09 +0100

    Revert "Use writers instead of packagePython3Script"
    This reverts commit 5c4dfe640e8843d7dd762f5fd89b479f0f335962.

--- packages/default.nix
+++ packages/default.nix
@@ -3,28 +3,21 @@
   flakeLib,
   inputs,
 }: let
-  # Usage: writePython3 "name" {environment} contents
-  # contents can come from builtins.readFile, but not a Git repository
-  # To use a git repo, add it as a flake input with flake = false.
-  writePython3Bin = pkgs.writers.writePython3Bin;
-
-  # Write a Python file without checking the syntax
-  writePythonFile = name: environment: file:
-    writePython3Bin name environment ''
-      # flake8: noqa
-      ${builtins.readFile file}
-    '';
+  packagePythonScript = flakeLib.package.packagePythonScript;
 in {
   # TODO: Add metadata
-  combine-blueprints = writePythonFile "combine-blueprints" {} ./combine-blueprints.py;
+  combine-blueprints = packagePythonScript "combine-blueprints" ./combine-blueprints.py "1.0.0";
 
   edit-config = import ./edit-config {inherit pkgs;};
 
   # TODO: Add metadata
-  export-blueprints = writePythonFile "export-blueprints" {} ./export-blueprints.py;
+  export-blueprints = packagePythonScript "export-blueprints" ./export-blueprints.py "1.0.0";
 
   # TODO: Add metadata
-  factorio-blueprint-decoder = writePythonFile "factorio-blueprint-decoder" {} "${inputs.src-factorio-blueprint-decoder}/decode";
+  factorio-blueprint-decoder = let
+    src = inputs.src-factorio-blueprint-decoder;
+  in
+    pkgs.writers.writePython3 "factorio-blueprint-decoder" {} (builtins.readFile "${src}/decode");
 
   nixvim = import ./nixvim {inherit pkgs inputs;};
 
