880facd95b3a20d0f06a333470da08f41c65e5fc
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-23 17:35:53 +0100

    Handle all MIME in one place

--- modules/home/mime/default.nix
+++ modules/home/mime/default.nix
@@ -39,18 +39,23 @@ in {
           definitionFile = "./definitions/application-x-foo.xml";
           defaultApp = "myapp.desktop";
         };
+        "application/pdf" = {
+          defaultApp = "firefox.desktop";
+        };
       };
 
       type = lib.types.attrsOf (lib.types.submodule {
         options = {
           definitionFile = lib.mkOption {
             description = "The path to the MIME definition file";
-            type = lib.types.path;
+            type = lib.types.nullOr lib.types.path;
+            default = null;
             example = "./definitions/application-x-foo.xml";
           };
           defaultApp = lib.mkOption {
             description = "The name of the *.desktop file to use as the default application";
-            type = lib.types.str;
+            type = lib.types.nullOr lib.types.str;
+            default = null;
             example = "myapp.desktop";
           };
         };
@@ -62,12 +67,14 @@ in {
 
   config = {
     # Copy definitions into the user's mime directory
-    home.file = lib.attrsets.mapAttrs' toHomeFileEntry config.custom.mime.definition;
+    home.file = lib.attrsets.mapAttrs' toHomeFileEntry
+      (lib.attrsets.filterAttrs (name: value: value.definitionFile != null) config.custom.mime.definition);
 
     xdg.mimeApps = {
       enable = true;
 
-      associations.added = lib.attrsets.mapAttrs' toXdgAssociation config.custom.mime.definition;
+      associations.added = lib.attrsets.mapAttrs' toXdgAssociation
+        (lib.attrsets.filterAttrs (name: value: value.defaultApp != null) config.custom.mime.definition);
     };
 
     # Update the user's mime database when rebuilding
