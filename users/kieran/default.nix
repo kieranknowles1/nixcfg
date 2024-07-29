e637adea5a7074aad44ff10fa9dd99b7821f8f46
Kieran <kieranknowles11@hotmail.co.uk>
2024-07-29 13:14:25 +0100

    Use toml for user config

--- users/kieran/default.nix
+++ users/kieran/default.nix
@@ -3,6 +3,50 @@
   config,
 }: let
   isDesktop = config.custom.deviceType == "desktop";
+
+  baseConfig = builtins.fromTOML (builtins.readFile ./config.toml);
+  desktopConfig =
+    if isDesktop
+    then builtins.fromTOML (builtins.readFile ./config-desktop.toml)
+    else {};
+
+  # TODO: Put this in our library
+  /*
+  Deeply merges a list of sets into a single set.
+  If a key is present in multiple sets, the values are merged with the following rules:
+  - Sets are merged using this function
+  - Lists are concatenated
+  - All other values are taken from the first set they appear in
+
+  Note: It is assumed that repeated values are of the same type in all sets.
+
+  # Example
+  ```nix
+  deepMergeSets [
+    { a = 1; b = [2]; c = { d = 3; e = 4; }; };
+    { a = 2; b = [3]; c = { d = 5; f = 6; }; };
+  ] => {
+    a = 1; # First value is taken
+    b = [2 3]; # Lists are concatenated, in the order the sets are given
+    c = {
+      d = 3; # Sets are merged recursively, with the same rules
+      e = 4; # Unique keys are are kept as-is
+      f = 6;
+    };
+  };
+  ```
+  */
+  deepMergeSets = sets:
+    builtins.zipAttrsWith
+    (name: values: let
+      first = builtins.elemAt values 0;
+    in
+      if builtins.isAttrs first
+      then deepMergeSets values # Recurse into nested sets
+      else if builtins.isList first
+      then builtins.concatLists values # Concatenate lists
+      else first) # We don't want to merge other types, so give the first value precedence
+    sets;
 in {
   core = {
     displayName = "Kieran";
@@ -10,32 +54,8 @@ in {
     shell = pkgs.nushell;
   };
 
-  # Some of this depends on the host, so I'm not configuring it as toml
-  # otherwise it would be a bit messy to have some toml and some nix
   home = {
-    # TODO: Use TOML for this, have separate "desktopOnly" and "allSystems" files that are conditionally imporI'm
-    custom = {
-      userDetails = {
-        email = "kieranknowles11@hotmail.co.uk";
-        firstName = "Kieran";
-        surName = "Knowles";
-      };
-
-      docs-generate.enable = isDesktop;
-      edit-config.enable = isDesktop;
-
-      shortcuts.enable = isDesktop;
-
-      editor = {
-        default =
-          if isDesktop
-          then "code"
-          else "nvim";
-
-        neovim.enable = true;
-        vscode.enable = isDesktop;
-      };
-    };
+    custom = deepMergeSets [desktopConfig baseConfig];
 
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
