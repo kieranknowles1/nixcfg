7615c7279c848320f25d26c51a109cf07215b2fd
Kieran <kieranknowles11@hotmail.co.uk>
2024-08-09 23:42:48 +0100

    606#desktop: Move shortcuts.enable to default.nix
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    606 current  2024-08-09 23:43:45  24.11.20240805.4cf1a7a  6.6.44                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 2811 -> 2811 (10 paths added, 10 paths removed, delta +0, disk usage +272B).

--- modules/home/shortcuts/default.nix
+++ modules/home/shortcuts/default.nix
@@ -1,6 +1,21 @@
-{...}: {
+{lib, config, hostConfig, ...}: {
   imports = [
     ./hotkeys.nix
     ./palette.nix
   ];
+
+  options.custom.shortcuts = {
+    enable = lib.mkEnableOption "keyboard shortcuts and command palettes";
+  };
+
+  config = let
+    cfg = config.custom.shortcuts;
+  in lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = hostConfig.custom.deviceType == "desktop";
+        message = "Keyboard shortcuts are only available on desktop devices";
+      }
+    ];
+  };
 }
