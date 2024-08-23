d29d85b8e49ae5b142b11d8153c0ce844494230e
Kieran <kieranknowles11@hotmail.co.uk>
2024-08-23 13:54:05 +0100

    Replace Rest with a PrimitiveReader type

--- packages/openmw-luadata/src/reader.rs
+++ packages/openmw-luadata/src/reader.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 use std::fs::File;
-use std::io::Read;
-use std::str::Utf8Error;
+use std::io::{BufRead, BufReader, Read};
+use std::string::FromUtf8Error;
 
 use ordered_float::OrderedFloat;
 use thiserror::Error;
@@ -12,7 +12,7 @@ pub enum Error {
     Io(#[from] std::io::Error),
 
     #[error(transparent)]
-    Utf8(#[from] Utf8Error),
+    Utf8(#[from] FromUtf8Error),
 
     #[error("Invalid data: {0}")]
     InvalidData(String),
@@ -24,7 +24,6 @@ impl Error {
     }
 }
 
-
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub const FORMAT_VERSION: u8 = 0x00;
@@ -32,94 +31,94 @@ pub const FORMAT_VERSION: u8 = 0x00;
 /// Decode a lua storage file in OpenMW's format
 /// NOTE: Assumes that values are encoded in little-endian
 pub fn decode(file: &str) -> Result<Value> {
-    let mut file = File::open(&file)?;
-    let mut data = Vec::new();
-    file.read_to_end(&mut data)?;
+    let file = File::open(&file)?;
+    let mut reader = PrimitiveReader::new(file);
 
-    if data.len() < 1 {
+    if reader.at_end()? {
         Err(Error::data("File is empty"))?;
     }
 
-    let Rest (rest, version) = read_u8(&data)?;
+    let version = reader.u8()?;
     if version != FORMAT_VERSION {
         Err(Error::data(
             &format!("Invalid format version: 0x{:02X}, expected 0x{:02X}", version, FORMAT_VERSION))
         )?;
     }
 
-    let Rest (unused, value) = Value::decode(rest)?;
+    let value = Value::decode(&mut reader)?;
 
-    if !unused.is_empty() {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidData,
-            "Trailing data",
-        ))?;
+    if !reader.at_end()? {
+        Err(Error::data("Trailing data"))?;
     }
 
     Ok(value)
 }
 
-/// A value, and the unparsed data that follows it
-/// The lifetime parameter `'a` enforces that the borrowed slice is valid for at least as long as
-/// the `Rest` struct
-struct Rest<'a, T>(&'a [u8], T);
+struct PrimitiveReader<T: Read> {
+    reader: BufReader<T>,
+}
 
-fn check_available(data: &[u8], size: usize) -> Result<()> {
-    if data.len() < size {
-        Err(Error::data(
-            &format!("Tried to read {} bytes, but only {} bytes are available", size, data.len())
-        ))?;
+/// Helper to read and decode Lua primitives
+/// Expects all data to be little-endian
+impl<T: Read> PrimitiveReader<T> {
+    fn new(reader: T) -> Self {
+        Self {
+            reader: BufReader::new(reader),
+        }
     }
 
-    Ok(())
-}
-
-/// Read a fixed-size slice from the data, or return an error if it's not available
-fn checked_read<const SIZE: usize>(data: &[u8]) -> Result<Rest<&[u8; SIZE]>> {
-    check_available(data, SIZE)?;
-    // This is safe because we just checked that the slice is long enough, and the size is fixed
-    let bytes = data[..SIZE].try_into().unwrap();
-    Ok(Rest(&data[SIZE..], bytes))
-}
-
-/// Read a slice of a given size from the data, or return an error if it's not available
-/// Use checked_read instead if the size is known at compile time, as the compiler knows
-/// more about the size and should produce faster/safer code
-fn checked_runtime_read(data: &[u8], size: usize) -> Result<Rest<&[u8]>> {
-    check_available(data, size)?;
-    Ok(Rest(&data[size..], &data[..size]))
-}
-
-fn read_u8(data: &[u8]) -> Result<Rest<u8>> {
-    let Rest (data, value): Rest<&[u8; 1]> = checked_read(data)?;
-
-    Ok(Rest(data, value[0]))
-}
-
-fn read_u32(data: &[u8]) -> Result<Rest<u32>> {
-    let Rest (data, value): Rest<&[u8; 4]> = checked_read(data)?;
-
-    let int = u32::from_le_bytes(*value);
-
-    Ok(Rest(data, int))
-}
-
-fn peek_u8(data: &[u8]) -> Result<u8> {
-    if data.is_empty() {
-        Err(Error::data(
-            "Unexpected end of data"
-        ))?;
+    /// Check if we're at the end of the file
+    /// Uses a mutable reference as we may need to fill the buffer
+    /// Does not consume the buffer
+    fn at_end(&mut self) -> Result<bool> {
+        Ok(self.reader.fill_buf()?.is_empty())
     }
 
-    Ok(data[0])
-}
+    /// Read a single byte
+    /// Consumes 1 byte
+    fn u8(&mut self) -> Result<u8> {
+        let mut buf = [0u8; 1];
+        self.reader.read_exact(&mut buf)?;
 
-fn read_f64(data: &[u8]) -> Result<Rest<f64>> {
-    let Rest (data, value): Rest<&[u8; 8]> = checked_read(data)?;
+        Ok(buf[0])
+    }
 
-    let float = f64::from_le_bytes(*value);
+    /// Read a single byte without consuming the buffer
+    fn peek_u8(&mut self) -> Result<u8> {
+        let buf = self.reader.fill_buf()?;
+        if buf.is_empty() {
+            Err(Error::data("Unexpected EOF"))?;
+        }
 
-    Ok(Rest(data, float))
+        Ok(buf[0])
+    }
+
+    /// Read a 32-bit unsigned integer
+    /// Consumes 4 bytes
+    fn u32(&mut self) -> Result<u32> {
+        let mut buf = [0u8; 4];
+        self.reader.read_exact(&mut buf)?;
+
+        Ok(u32::from_le_bytes(buf))
+    }
+
+    /// Read a double-precision float
+    /// Consumes 8 bytes
+    fn f64(&mut self) -> Result<f64> {
+        let mut buf = [0u8; 8];
+        self.reader.read_exact(&mut buf)?;
+
+        Ok(f64::from_le_bytes(buf))
+    }
+
+    /// Read a fixed-size string
+    /// Consumes size bytes
+    fn string(&mut self, size: usize) -> Result<String> {
+        let mut buf = vec![0u8; size];
+        self.reader.read_exact(&mut buf)?;
+
+        Ok(String::from_utf8(buf)?)
+    }
 }
 
 // We use a btree instead of a hashmap to ensure that the keys are sorted
@@ -133,7 +132,7 @@ type Table = BTreeMap<Value, Value>;
 #[derive(Debug)]
 pub enum Value {
     // Rust doesn't let us have ordered floats, as NaN causes ambiguity. We instead use the
-    // OrderedFloat crate which wraps a f64 to implement the Ord traI'm
+    // OrderedFloat crate which wraps a f64 to implement the Ord trait
     Number(OrderedFloat<f64>),
     Boolean(bool),
     String(String),
@@ -155,46 +154,40 @@ const T_VEC2: u8 = 0x10;
 const MASK_SHORT_STRING: u8 = 0x1F;
 
 impl Value {
-    fn decode(data: &[u8]) -> Result<Rest<Value>> {
-        let Rest (data, tag) = read_u8(data)?;
+    /// Decode a value from the reader
+    /// Consumes as much data as needed to decode the value
+    /// Recurses into tables
+    fn decode<T: Read>(reader: &mut PrimitiveReader<T>) -> Result<Self> {
+        let tag = reader.u8()?;
 
         match tag {
             T_NUMBER => {
-                let Rest (data, number) = read_f64(data)?;
-                Ok(Rest(data, Self::Number(OrderedFloat(number))))
+                let number = reader.f64()?;
+                Ok(Self::Number(OrderedFloat(number)))
             },
             T_LONG_STRING => {
-                let Rest (data, length) = read_u32(data)?;
-                let length = length as usize;
-                let Rest (data, string) = checked_runtime_read(data, length)?;
-                let string = std::str::from_utf8(string)?;
-
-                Ok(Rest(data, Self::String(string.to_string())))
+                let length = reader.u32()? as usize;
+                let string = reader.string(length)?;
+                Ok(Self::String(string))
             },
             T_BOOLEAN => {
-                let Rest (data, value) = read_u8(data)?;
-                Ok(Rest(data, Self::Boolean(value != 0)))
+                let value = reader.u8()?;
+                Ok(Self::Boolean(value != 0))
             },
             T_TABLE_START => {
-                let Rest(data, table) = Self::decode_table(data)?;
-                Ok(Rest(data, Self::Table(table)))
+                let table = Self::decode_table(reader)?;
+                Ok(Self::Table(table))
             },
-            T_TABLE_END => {
-                // The only legal place for TABLE_END is in place of a table key
-                // This function should never be asked to decode it, as it's handled by decode_table
-                Err(Error::data(
-                    "Unexpected table end"
-                ))
-            }
             T_VEC2 => {
-                let Rest (data, x) = read_f64(data)?;
-                let Rest (data, y) = read_f64(data)?;
-                Ok(Rest(data, Self::Vec2(OrderedFloat(x), OrderedFloat(y))))
+                let x = reader.f64()?;
+                let y = reader.f64()?;
+                Ok(Self::Vec2(OrderedFloat(x), OrderedFloat(y)))
             },
-            0x20..=0x3f => {
+            // Every bit after the flag is part of the length, so we can use a range and mask
+            0x20..=0x3F => {
                 let length = (tag & MASK_SHORT_STRING) as usize;
-                let string = std::str::from_utf8(&data[..length])?;
-                Ok(Rest(&data[length..], Self::String(string.to_string())))
+                let string = reader.string(length)?;
+                Ok(Self::String(string))
             },
             _ => {
                 Err(Error::data(&format!("Unknown tag: 0x{:02X}", tag)))
@@ -202,25 +195,22 @@ impl Value {
         }
     }
 
-    fn decode_table(data: &[u8]) -> Result<Rest<Table>> {
+    /// Decode a table from the reader
+    /// Assumes that the buffer points to the first byte after TABLE_START
+    fn decode_table<T: Read>(reader: &mut PrimitiveReader<T>) -> Result<Table> {
         let mut table = Table::new();
-        let mut data = data;
 
-        // Decode key-value pairs until we find a terminator
+        // Read until we see TABLE_END in place of the key's type
         loop {
-            let tag = peek_u8(data)?;
+            let tag = reader.peek_u8()?;
             if tag == T_TABLE_END {
-                return Ok(Rest(&data[1..], table));
+                reader.u8()?; // Consume the TABLE_END
+                return Ok(table);
             }
 
-            // Make sure we advance the data pointer after each read
-            let key_pair = Self::decode(data)?;
-            data = key_pair.0;
-            let key = key_pair.1;
-
-            let value_pair = Self::decode(data)?;
-            data = value_pair.0;
-            let value = value_pair.1;
+            // Table keys can be any type
+            let key = Self::decode(reader)?;
+            let value = Self::decode(reader)?;
             table.insert(key, value);
         }
     }
