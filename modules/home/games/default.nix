064faaa679122837587bb7c11b30df08a7d1d0e5
Kieran <kieranknowles11@hotmail.co.uk>
2024-08-29 15:49:02 +0100

    Add secret for nexus mods key

--- modules/home/games/default.nix
+++ modules/home/games/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  config,
   hostConfig,
   ...
 }: {
@@ -9,10 +10,22 @@
     ./skyrim
   ];
 
+  options.custom.games = {
+    nexusModsKey = lib.mkOption {
+      type = lib.types.str;
+      description = ''
+        Secret containing the API key for Nexus Mods.
+      '';
+      example = "nexusmods/apikey";
+    };
+  };
+
   config = lib.mkIf hostConfig.custom.games.enable {
     home.packages = with pkgs; [
       # Minecraft launcher
       prismlauncher
     ];
+
+    sops.secrets."nexusmods/apikey".key = config.custom.games.nexusModsKey;
   };
 }
