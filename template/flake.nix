611f5b5b34b9a3bd2b65957b87b5450b479f2802
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-09 16:33:00 +0100

    Add template flake

---
+++ template/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A flake based on my NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    nixcfg = {
+      url = "github:kieranknowles1/nixcfg";
+      inputs.nixpkgs-unstable.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }@inputs: let
+    eachDefaultSystem = inputs.flake-utils.lib.eachDefaultSystem;
+    cfgLib = inputs.nixcfg.lib;
+  in eachDefaultSystem (system: let
+    pkgs = import inputs.nixpkgs { inherit system; };
+  in {
+    # Per system type
+    packages = {
+
+    };
+
+    devShells = {
+      # Wrapper that sets the magic DEVSHELL variable, and preserves the user's default shell
+      default = cfgLib.shell.mkShellEx {
+        name = "dev";
+        packages = with pkgs; [
+          hello
+        ];
+
+        shellHook = ''
+          hello
+        '';
+      };
+    };
+  }) // {
+    # Shared across all systems
+  };
+}
