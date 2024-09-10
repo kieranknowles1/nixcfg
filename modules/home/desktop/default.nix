1a269f072bad49516329874c72b5ed583ef2eed5
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-10 23:21:26 +0100

    648#rocinante: Manage templates
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    648 current  2024-09-10 23:22:18  24.11.20240910.821472b  6.6.50                          *
    
    <<< /run/current-system
    >>> result
    Added packages:
    [A.]  #1  hm_templates  <none>
    Closure size: 2779 -> 2780 (10 paths added, 9 paths removed, delta +1, disk usage +528B).

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,5 +1,18 @@
-{...}: {
+{
+  lib,
+  config,
+  hostConfig,
+  ...
+}: let
+  isDesktop = hostConfig.custom.deviceType == "desktop";
+in {
   imports = [
     ./gnome.nix
   ];
+
+  config = lib.mkIf isDesktop {
+    # Add templates for creating new files
+    # When creating from a template, the file will be copied from ~/Templates to the current directory
+    home.file.${config.xdg.userDirs.templates}.source = ./templates;
+  };
 }
