2be6ad30b4549287b687d9222273bdee366d3bb1
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-14 23:51:41 +0100

    Avoid duplicate flake inputs

--- modules/nixos/desktop/cosmic.nix
+++ modules/nixos/desktop/cosmic.nix
@@ -7,8 +7,8 @@
 in {
   config = lib.mkIf isCosmic {
     nix.settings = {
-      substituters = [ "https://cosmic.cachix.org/" ];
-      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+      substituters = ["https://cosmic.cachix.org/"];
+      trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
     };
 
     services.desktopManager.cosmic = {
