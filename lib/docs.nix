3809ea04cee15549d9054301cd3020a36598b26e
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-22 22:46:02 +0100

    Remove /nix/store from generated docs

--- lib/docs.nix
+++ lib/docs.nix
@@ -83,11 +83,19 @@ in {
   mkOptionDocs = importer: let
     modulesEval = evalModules importer;
 
-    optionsDoc = pkgs.nixosOptionsDoc {
+    # File containing options documentation
+    optionsDoc = (pkgs.nixosOptionsDoc {
       options = modulesEval.options;
-    };
-  in
-    optionsDoc.optionsCommonMark;
+    }).optionsCommonMark;
+
+    sed = lib.getExe pkgs.gnused;
+    # Nix regexes only support complete matches, so we can't easily match the store path.
+    # Instead, we'll remove it with sed.
+    # Use pipe as a delimiter so we don't have to escape slashes.
+    # TODO: Can we make the link point to the repository?
+  in pkgs.runCommand "option-docs.md" {} ''
+    cat ${optionsDoc} | ${sed} --regexp-extended 's|\/nix/store/[a-z0-9]{32}-source/||g' > $out
+  '';
 
   /*
   *
