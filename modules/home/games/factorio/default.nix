90a1b177aee35f9c380ca410ded6f240b938708e
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-26 00:19:42 +0100

    Sync openmw lua settings

--- modules/home/games/factorio/default.nix
+++ modules/home/games/factorio/default.nix
@@ -16,6 +16,6 @@ in {
   # There's currently no way to create a blueprint binary directly, so we provide a string
   # that can be imported into Factorio.
   config = lib.mkIf hostConfig.custom.games.enable {
-    home.file."Games/factorio-blueprints.txt".source = blueprintString;
+    home.file."Games/configs/factorio-blueprints.txt".source = blueprintString;
   };
 }
