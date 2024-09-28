27f6c0af1959839b4ce233ee64f724aac399e74f
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-28 18:08:38 +0100

    Use flake-parts for shells

--- shells/default.nix
+++ shells/default.nix
@@ -1,8 +1,21 @@
-{pkgs}: let
-  callPackage = pkgs.callPackage;
-in {
-  # `default.nix` is already used for this file, so use a different name
-  default = callPackage ./defaultShell.nix {};
+{
+  inputs,
+  self,
+  ...
+}: {
+  perSystem = {system, ...}: let
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+      # TODO: Remove this once flake-parts has a proper way of handling overlays
+      overlays = builtins.attrValues self.overlays;
+    };
+    callPackage = pkgs.callPackage;
+  in {
+    devShells = {
+      # `default.nix` is already used for this file, so use a different name
+      default = callPackage ./defaultShell.nix {};
 
-  rust = callPackage ./rust.nix {};
+      rust = callPackage ./rust.nix {};
+    };
+  };
 }
