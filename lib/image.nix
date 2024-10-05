852ea2ad00b1f7517f78cea8771b6f5c1286d52a
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-05 23:34:24 +0100

    Make the factorio blueprint dir configurable

--- lib/image.nix
+++ lib/image.nix
@@ -25,18 +25,8 @@
   source :: Path
   : The path to the HEIC image to convert.
   */
-  fromHeif = source: let
-    # runCommand creates a derivation that runs the given script.
-    # It requires us to create a directory at $out, the path to which
-    # is returned by the expression and can therefore be used to output
-    # files.
-    # PNGs don't compress very well with photos, but I don't want a function
-    # doing anything lossy without being explicit about it.
-    convert = pkgs.runCommand "heif-to-png" {} ''
-      mkdir -p $out
-      # NOTE: This will need to be changed to heif-dec once 1.18 is on stable
-      ${pkgs.libheif}/bin/heif-dec "${source}" "$out/output.png"
+  fromHeif = source:
+    pkgs.runCommand "image.png" {} ''
+      ${pkgs.libheif}/bin/heif-dec "${source}" "$out"
     '';
-    # We only care about the image, not the directory it's in.
-  in "${convert}/output.png";
 }
