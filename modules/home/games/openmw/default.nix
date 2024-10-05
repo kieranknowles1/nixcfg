ce359cfe438d9234b4ce55c9af9ae4204d64691c
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-05 23:39:05 +0100

    Make OpenMW storage configurable

--- modules/home/games/openmw/default.nix
+++ modules/home/games/openmw/default.nix
@@ -11,15 +11,30 @@
   toOmwData = json: let
     converter = lib.getExe cfg.luaData.package;
   in
-    pkgs.runCommand "omw-lua-convert" {} ''
+    pkgs.runCommand "omw-lua.bin" {} ''
       ${converter} encode --input ${json} --output $out
     '';
 in {
-  options.custom.games.openmw = {
+  # TODO: Store data with the user, as in Factorio
+  options.custom.games.openmw = let
+    mkStorageOption = name: default: defaultText: lib.mkOption {
+      description = ''
+        The JSON file containing ${name} storage data,
+        as exported using the OpenMW LuaData tool.
+      '';
+      type = lib.types.path;
+      inherit default defaultText;
+    };
+  in {
     luaData.package = lib.mkPackageOption pkgs.flake "openmw-luadata" {};
+
+    globalStorage = mkStorageOption "global" ./global_storage.json "./global_storage.json";
+    playerStorage = mkStorageOption "player" ./player_storage.json "./player_storage.json";
   };
 
-  config = lib.mkIf hostConfig.custom.games.enable {
+  config = let
+    cfg = config.custom.games.openmw;
+  in lib.mkIf hostConfig.custom.games.enable {
     home = {
       packages = with pkgs; [
         flake.openmw-dev
@@ -28,8 +43,8 @@ in {
       # TODO: Use an impure provisioner to install Lua data files directly to ~/.config/openmw
       # TODO: Consider syncing rest my openmw config
       file = {
-        "Games/configs/openmw/global_storage.dat".source = toOmwData ./global_storage.json;
-        "Games/configs/openmw/player_storage.dat".source = toOmwData ./player_storage.json;
+        "Games/configs/openmw/global_storage.dat".source = toOmwData cfg.globalStorage;
+        "Games/configs/openmw/player_storage.dat".source = toOmwData cfg.playerStorage;
       };
     };
   };
