af8cb998ea6de049a93d1d1eaa53c76f9350ef3b
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-08 23:46:52 +0100

    Open empty files in vs code

--- modules/home/editor/default.nix
+++ modules/home/editor/default.nix
@@ -34,21 +34,22 @@
 
       # Taken from nvim.desktop
       default = [
+        "application/x-shellscript"
+        "application/x-zerosize"
         "text/english"
         "text/plain"
-        "text/x-makefile"
+        "text/x-c"
+        "text/x-c++"
         "text/x-c++hdr"
         "text/x-c++src"
         "text/x-chdr"
         "text/x-csrc"
         "text/x-java"
+        "text/x-makefile"
         "text/x-moc"
         "text/x-pascal"
         "text/x-tcl"
         "text/x-tex"
-        "application/x-shellscript"
-        "text/x-c"
-        "text/x-c++"
       ];
     };
   };
