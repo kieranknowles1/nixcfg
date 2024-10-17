16fcf59b06aed5ec1064c1394046a0a7a05abe2d
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-18 00:07:20 +0100

    Disable template inputs by default

--- template/flake.nix
+++ template/flake.nix
@@ -5,12 +5,36 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     nixcfg = {
       url = "github:kieranknowles1/nixcfg";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
+      inputs.systems.follows = "systems";
+
+      # Disable inputs not needed outside of nixcfg
+      # These can be re-enabled if needed
+      inputs.clan-core.follows = "";
+      inputs.factorio-blueprints.follows = "";
+      inputs.firefox-addons.follows = "";
+      inputs.flake-utils-plus.follows = "";
+      inputs.flake-utils.follows = "";
+      inputs.home-manager.follows = "";
+      inputs.nix-index-database.follows = "";
+      inputs.nixos-cosmic.follows = "";
+      inputs.nixpkgs-openmw.follows = "";
+      inputs.nixpkgs-stable.follows = "";
+      inputs.nixvim.follows = "";
+      inputs.openmw.follows = "";
+      inputs.snowfall-lib.follows = "";
+      inputs.sops-nix.follows = "";
+      inputs.src-factorio-blueprint-decoder.follows = "";
+      inputs.stylix.follows = "";
+      inputs.vscode-extensions.follows = "";
     };
   };
 
--- template/readme.md
+++ template/readme.md
@@ -2,3 +2,5 @@
 
 This is a repository based on my
 [NixOS Configuration](https://github.com/kieranknowles1/nixcfg).
+
+See comments in [flake.nix](./flake.nix) for more information.
