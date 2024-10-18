4c5ffb9b5d9466e9b306655041eafa9e47df30b8
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-18 22:48:58 +0100

    Fix shellcheck error

--- packages/nix-utils/venv.sh
+++ packages/nix-utils/venv.sh
@@ -4,5 +4,12 @@
 
 venv="${1:-venv}"
 
+if [ ! -d "$venv" ]; then
+  echo "Could not find virtual environment: $venv"
+  return 1
+fi
+
+# This script is meant to be used anywhere. We check for the venv above.
+# shellcheck disable=SC1091
 source "$venv/bin/activate" # Activate the virtual environment. This exports all needed variables.
 exec "$SHELL" # Return to the user's preferred shell.
