72c0feb234ba9cbb93941e9e606d3084ad1c550b
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-20 18:38:02 +0100

    Basic restore functionality

--- packages/activate-mutable/src/main.rs
+++ packages/activate-mutable/src/main.rs
@@ -1,11 +1,21 @@
 mod activate;
 mod config;
+mod restore;
 
 use std::process::ExitCode;
 
 use clap::Parser;
+use thiserror::Error;
 
-type Result<T> = activate::Result<T>;
+#[derive(Error, Debug)]
+enum Error {
+    #[error("Activate error: {0}")]
+    Activate(#[from] activate::Error),
+    #[error("Restore error: {0}")]
+    Restore(#[from] restore::Error),
+}
+
+type Result<T> = std::result::Result<T, Error>;
 
 // [[../../../docs/plan/activate-mutable.md]]
 // TODO: V2: Restore files to repo
@@ -14,11 +24,13 @@ type Result<T> = activate::Result<T>;
 #[derive(Parser)]
 enum Opt {
     Activate(activate::Opt),
+    Restore(restore::Opt),
 }
 
 fn main() -> Result<ExitCode> {
     let any_errors = match Opt::parse() {
         Opt::Activate(args) => activate::run(args)?,
+        Opt::Restore(args) => restore::run(args)?,
     };
 
     // If any errors occurred, return a non-zero exit code.
