618f808948c4008f200aa4f530fbfdd3e061cbc7
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-20 18:04:40 +0100

    Move read_config to config.rs

--- packages/activate-mutable/src/config.rs
+++ packages/activate-mutable/src/config.rs
@@ -1,10 +1,31 @@
 use std::collections::BTreeMap;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("I/O error {0}")]
+    Io(#[from] std::io::Error),
+    #[error("JSON error {0}")]
+    Json(#[from] serde_json::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 // use a BTreeMap to keep the order predictable
 pub type Config = BTreeMap<PathBuf, ConfigEntry>;
+
+pub fn get_previous_config_path(home: &Path) -> PathBuf {
+    home.join(".config/activate-mutable-config.json")
+}
+
+pub fn read_config(file: &Path) -> Result<Config> {
+    let file = std::fs::File::open(file)?;
+    let json: Config = serde_json::from_reader(file)?;
+    Ok(json)
+}
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
@@ -15,8 +36,13 @@ pub enum ConflictStrategy {
 
 #[derive(Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
+#[serde(rename_all = "camelCase")]
 pub struct ConfigEntry {
+    // Absolute path to the file, typically in the Nix store.
     pub source: PathBuf,
-    #[serde(rename = "onConflict")]
+    // What to do when the file has changed locally.
     pub on_conflict: ConflictStrategy,
+    // Path to the file in the repository, relative to the repository root.
+    // TODO: Should this be required?
+    pub repo_path: Option<String>,
 }
