bc9a4a568961440e658dbfa2ea0e0a2c98dc94ab
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-25 20:12:37 +0100

    Use args instead of environ vars

--- checks/default.nix
+++ checks/default.nix
@@ -1,21 +1,35 @@
 {self, ...}: {
   perSystem = {pkgs, ...}: {
-    checks = {
-      duplicate-input = pkgs.runCommand "check-duplicate-input" {
-        nativeBuildInputs = with pkgs; [jq];
-        LOCK_FILE = "${self}/flake.lock";
-      } (builtins.readFile ./duplicate-input.sh);
-
-      markdown-links =
-        pkgs.runCommand "check-markdown-links" {
-          nativeBuildInputs = with pkgs; [nodePackages.markdown-link-check];
-        } ''
-          # Don't check links to external sites, as Nix builds are meant to be reproducible
-          # and external sites can change their content.
-          # Also ignore files in ./docs/generated, as they are assumed to be OK.
-          bash ${./markdown-links.sh} ${./markdown-link-config.json} ${self}
+    checks = let
+      # Make a check that will succeed if the script exits 0, and fail otherwise.
+      mkCheck = {
+        nativeBuildInputs ? [],
+        script,
+        name,
+        args ? [],
+      }:
+        pkgs.runCommand name {inherit nativeBuildInputs;} ''
+          set -e
+          bash ${script} ${builtins.concatStringsSep " " args}
           touch $out
         '';
+    in {
+      duplicate-input = mkCheck {
+        nativeBuildInputs = with pkgs; [jq];
+        script = ./duplicate-input.sh;
+        name = "check-duplicate-input";
+        args = ["${self}/flake.lock"];
+      };
+
+      markdown-links = mkCheck {
+        nativeBuildInputs = with pkgs; [nodePackages.markdown-link-check];
+        script = ./markdown-links.sh;
+        name = "check-markdown-links";
+        # Don't check links to external sites, as Nix builds are meant to be reproducible
+        # and external sites can change their content.
+        # Also ignore files in ./docs/generated, as they are assumed to be OK.
+        args = [./markdown-link-config.json self];
+      };
     };
   };
 }
