4230df2ed63b2f09ef62880b603ad20e9a63d74d
Kieran <kieranknowles11@hotmail.co.uk>
2024-11-06 21:28:24 +0000

    Be more consise with logging

--- packages/export-notes/export-notes.sh
+++ packages/export-notes/export-notes.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
+# This is meant to be used from the command palette, so keep stdout brief.
+log() {
+  echo "$(date +'%H:%M:%S') $1"
+}
+
 API_ROOT="http://127.0.0.1:37840/etapi"
 API_KEY=$(cat ~/.local/share/trilium-data/token)
 DST_DIR=~/Documents/notes-export
 META_FILE="$DST_DIR/!!!meta.json"
 
 export_file=$(mktemp)
-echo "Feching notes from $API_ROOT to $export_file"
-curl --header "Authorization: $API_KEY" "$API_ROOT/notes/root/export" > "$export_file"
+log "Feching notes from $API_ROOT to $export_file"
+curl --header "Authorization: $API_KEY" "$API_ROOT/notes/root/export" > "$export_file" -Ss
 
-echo "Unzipping $export_file to $DST_DIR"
+log "Fetched $(du -k "$export_file" | cut -f1)KB of data"
+log "Unzipping $export_file to $DST_DIR"
 unzip -q -o "$export_file" -d "$DST_DIR"
 rm "$export_file"
 
-echo "Cleaning up export"
+log "Cleaning up export"
 
 # Discard the isExpanded property from all notes, as it is part of the UI state
 tmp_file=$(mktemp)
@@ -30,12 +36,13 @@ while IFS= read -r -d '' file; do
 done < <(find "$DST_DIR" -type f -name '*.json' -print0)
 
 
-echo "Committing changes to git"
+log "Committing changes to git"
 repo_clean=$(git -C "$DST_DIR" status --porcelain | wc -l)
 if [ "$repo_clean" -gt 0 ]; then
+  log "Changes detected, committing to git"
   git -C "$DST_DIR" add .
   git -C "$DST_DIR" commit -m "Update on $(date +'%Y-%m-%d %H:%M:%S')" > /dev/null
   # git -C "$DST_DIR" push
 else
-  echo "No changes since last export"
+  log "No changes since last export"
 fi
