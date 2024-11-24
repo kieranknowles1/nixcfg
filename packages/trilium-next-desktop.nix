49c8ef1e34fe1d0a1c6c8144d8a8b1b7c3281001
Kieran <kieranknowles11@hotmail.co.uk>
2024-11-24 22:40:53 +0000

    130#canterbury: Update trilium-next
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    130 current  2024-11-24 22:44:07  24.11.20241105.8c4dc69  6.6.59                          *
    
    <<< /run/current-system
    >>> result
    Version changes:
    [U.]  #1  trilium-desktop  0.90.11-beta -> 0.90.12
    Closure size: 2342 -> 2342 (14 paths added, 14 paths removed, delta +0, disk usage +160.1KiB).

--- packages/trilium-next-desktop.nix
+++ packages/trilium-next-desktop.nix
@@ -4,10 +4,10 @@
 }:
 trilium-desktop.overrideAttrs (oldAttrs: rec {
   # TODO: Run this on a server
-  version = "0.90.11-beta";
+  version = "0.90.12";
   src = builtins.fetchurl {
     url = "https://github.com/TriliumNext/Notes/releases/download/v${version}/TriliumNextNotes-v${version}-linux-x64.zip";
-    sha256 = "sha256:0cijj3nc67saiydh1mqafp6y8fljkhwxyqjhzfiyg7275244sfng";
+    sha256 = "sha256:0ji28l60wyzhjbi6g5845dnm763bvg7535zfgzcmfgwjs6zr6nfq";
   };
   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [unzip];
 })
