68cafee8f18a172ecfbf37b8ef176db5d58bcc24
Kieran <kieranknowles11@hotmail.co.uk>
2024-11-30 15:04:30 +0000

    783#rocinante: confbuild helper
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    783 current  2024-11-30 15:06:06  25.05.20241126.af51545  6.6.63                          *
    
    <<< /run/current-system
    >>> result
    No version or selection state changes.
    Closure size: 3282 -> 3282 (25 paths added, 25 paths removed, delta +0, disk usage +1.9KiB).

---
+++ packages/nix-utils/confbuild.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+if [ "$#" -ne 2 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: $0 [home|nixos] <option_path>"
+    if [ "$1" != "-h" ] && [ "$1" != "--help" ]; then
+        exit 1
+    else
+        exit 0
+    fi
+fi
+
+mode="$1"
+option_path="$2"
+
+prefix=".#nixosConfigurations.$(hostname).config"
+
+if [ "$mode" == "home" ] || [ "$mode" == "h" ]; then
+    prefix="$prefix.home-manager.users.$USER.custom"
+elif [ "$mode" == "nixos" ] || [ "$mode" == "n" ]; then
+    prefix="$prefix.custom"
+else
+    echo "Invalid mode: $mode"
+    exit 1
+fi
+
+exec nix build "$prefix.$option_path"
