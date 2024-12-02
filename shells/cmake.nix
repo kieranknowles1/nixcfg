d3f38fb7fe90061cf44ff981134bad43dec89681
Kieran <kieranknowles11@hotmail.co.uk>
2024-12-02 11:42:26 +0000

    Add cmake shell

---
+++ shells/cmake.nix
@@ -0,0 +1,31 @@
+{
+  cmake,
+  clang,
+  flake,
+  # Bring our own CC, I prefer clang to gcc
+  mkShellNoCC,
+
+  # Best balance between speed and debuggability
+  buildType ? "RelWithDebInfo",
+  # Additional libraries and tools to include
+  libraries ? [],
+}:
+flake.lib.shell.mkShellEx mkShellNoCC {
+  name = "cmake";
+
+  packages = [ cmake clang ] ++ libraries;
+
+  CMAKE_FLAGS = "-DCMAKE_BUILD_TYPE=${buildType}";
+
+  shellHook = ''
+    # mkcd into build if not already
+    if [[ "$PWD" != *build ]]; then
+      mkdir -p "build"
+      cd "build"
+    fi
+
+    cmake ..
+    code ..
+    cd ..
+  '';
+}
