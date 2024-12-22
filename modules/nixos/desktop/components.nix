2cec9cb01b3ccc5908783790ab59a702b23492c2
Kieran <kieranknowles11@hotmail.co.uk>
2024-12-22 00:51:35 +0000

    Remove extra .desktop files from thunar

--- modules/nixos/desktop/components.nix
+++ modules/nixos/desktop/components.nix
@@ -1,14 +1,23 @@
 # Common components shared between desktop environments
-{pkgs, ...}: {
-  config.environment = {
+{pkgs, ...}: let
+  thunar-onedesktop = pkgs.xfce.thunar.overrideAttrs (oldAttrs: {
+    postFixup = ''
+      # Remove the .desktop files for bulk rename and settings to reduce clutter
+      rm $out/share/applications/thunar-bulk-rename.desktop
+      rm $out/share/applications/thunar-settings.desktop
+    '';
+  });
+in {
+  config = {
     # Nautilus has been acting up lately, plus it struggles with large directories
     # and isn't to my liking. Thunar meets my one requirement: it's fast. Nautilus
     # takes 1sec to open a directory with 150 files, Thunar can do 2000 in 100ms.
+    environment = {
     gnome.excludePackages = with pkgs; [
       nautilus
     ];
     systemPackages = with pkgs; [
-      xfce.thunar
+        thunar-onedesktop
       xfce.xfconf
     ];
   };
