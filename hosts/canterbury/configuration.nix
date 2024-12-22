517e2264c0e64021918f75927643a74385163bbb
Kieran <kieranknowles11@hotmail.co.uk>
2024-12-22 15:39:04 +0000

    Revert "Use symlink for configuration.nix"
    Oops it doesn't work
    This reverts commit 65bc6c25a2eeb23ca0f713013c1b400e6763fc50.

--- hosts/canterbury/configuration.nix
+++ hosts/canterbury/configuration.nix
@@ -1,1 +1,29 @@
-../../truths/default-configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  pkgs,
+  config,
+  self,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    {
+      custom = {
+        user.kieran = import "${self}/users/kieran" {inherit pkgs config self;};
+
+        secrets = {
+          ageKeyFile = "/home/kieran/.config/sops/age/keys.txt";
+          file = ./secrets.yaml;
+        };
+      };
+    }
+  ];
+
+  # Enable everything needed for this configuration
+  config.custom = self.lib.host.readTomlFile ./config.toml;
+
+  # TODO: Have a proper option for this
+  config.programs.kdeconnect.enable = true;
+}
