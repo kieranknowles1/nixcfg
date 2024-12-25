16f216a0518e53c65e3549002d96d1dc45048877
Kieran <kieranknowles11@hotmail.co.uk>
2024-12-25 23:52:37 +0000

    Declare font options where they are used

--- modules/home/nushell/default.nix
+++ modules/home/nushell/default.nix
@@ -5,9 +5,23 @@
   config,
   hostConfig,
   lib,
+  pkgs,
   ...
 }: {
-  config = {
+  options.custom = let
+    inherit (lib) mkPackageOption;
+  in {
+    fonts = {
+      defaultMono = mkPackageOption pkgs.nerd-fonts "dejavu-sans-mono" {
+        extraDescription = "Default monospace font";
+      };
+    };
+  };
+
+  config = let
+    cfg = config.custom;
+    isDesktop = hostConfig.custom.features.desktop;
+  in {
     # TODO: Link to the shell set on the host side, a NuShell specific file isn't the best place for this
     xdg.configFile."default-shell".source = lib.getExe config.programs.nushell.package;
 
@@ -17,7 +31,7 @@
 
         # Give us an environment variable for our flake path
         environmentVariables = {
-          FLAKE = config.custom.fullRepoPath;
+          FLAKE = cfg.fullRepoPath;
         };
 
         # Append my custom config to the default
@@ -43,12 +57,23 @@
       };
     };
 
-    # fontconfig is not relevant on servers
-    fonts.fontconfig.enable = hostConfig.custom.features.desktop;
+    # Install NerdFonts, as required by Starship and several other programs
+    # Not relevant on servers, as SSH fonts are handled by the client
+    fonts.fontconfig = {
+      enable = isDesktop;
 
-    # Starship uses icons from NerdFonts
-    home.packages = [
-      config.custom.fonts.defaultMono
-    ];
+      defaultFonts = {
+        monospace = lib.singleton (
+          # FIXME: New nixpkgs doesn't list the font name in the package name
+          if cfg.fonts.defaultMono == pkgs.nerd-fonts.dejavu-sans-mono
+          then "DejaVuSansMono"
+          else builtins.throw "Unknown default monospace font"
+        );
+      };
+    };
+
+    home.packages =
+      lib.optional isDesktop
+      config.custom.fonts.defaultMono;
   };
 }
