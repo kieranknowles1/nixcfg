067c1ddb448bd325b6790af0f0885df4c4a2af0a
Kieran <kieranknowles11@hotmail.co.uk>
2024-12-30 22:55:15 +0000

    Revert "Rewrite buildStaticSite buildPhase in nushell"
    This reverts commit 568814961cf92f7922ea96fd1e0664210475cdf8.

--- builders/default.nix
+++ builders/default.nix
@@ -26,7 +26,7 @@
         builders = let
           callPackage = lib.customisation.callPackageWith (pkgs // inputs);
         in {
-          buildStaticSite = callPackage ./buildStaticSite {};
+          buildStaticSite = callPackage ./buildStaticPhp {};
           buildScript = callPackage ./buildScript.nix {};
           fromHeif = callPackage ./fromHeif.nix {};
           mkOptionDocs = callPackage ./mkOptionDocs.nix {};
