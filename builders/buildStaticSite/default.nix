c6d2ff4bc9cc6c5c3e81bd8225e2dd12553afa28
Kieran <kieranknowles11@hotmail.co.uk>
2024-12-31 11:09:44 +0000

    Build html docs

--- builders/buildStaticSite/default.nix
+++ builders/buildStaticSite/default.nix
@@ -27,9 +27,33 @@ stdenv.mkDerivation (args
       # For Pandoc reproducibility
       export SOURCE_DATE_EPOCH=0
 
+      buildPandoc() {
+        file="$1"
+        out_relative="$2"
+
+        # Grab the title from the first line of the file
+        title=$(head -n 1 "$file" | sed 's|^# ||')
+
+        # Remove the title as Pandoc will add it back
+        tmpfile=$(mktemp)
+        tail -n +2 "$file" > $tmpfile
+
+        pandoc \
+          --standalone \
+          --from markdown --to html \
+          --css style.css \
+          --metadata title="$title" \
+          --fail-if-warnings \
+          "$tmpfile" --output "$out/$(basename $out_relative .md).html"
+        rm "$tmpfile"
+
+        # Replace .md with .html in links. Assumes that .md is always followed by a quote for the end of href=""
+        sed -i 's|\.md"|\.html"|g' "$out/$(basename $out_relative .md).html"
+      }
+
       mkdir -p $out
       while IFS= read -r -d "" file; do
-        relative=$(realpath --relative-to=$src $file)
+        relative=$(realpath --no-symlinks --relative-to=$src $file)
         out_relative=$out/$relative
 
         # Exclude files in .build-only
@@ -37,23 +61,20 @@ stdenv.mkDerivation (args
           continue
         fi
 
+        echo "Processing $file"
+
         mkdir -p $(dirname $out_relative)
 
         if [[ "$file" == *.php ]]; then
           php -f ${./buildFile.php} "$file" > "$out/$(basename $out_relative .php)".html
         elif [[ "$file" == *.md ]]; then
-          pandoc \
-            --standalone \
-            --css style.css \
-            --fail-if-warnings \
-            "$file" --output "$out/$(basename $out_relative .md).html"
-
-          # Replace .md with .html in links. Assumes that .md is always followed by a quote for the end of href=""
-          sed -i 's|\.md"|\.html"|g' "$out/$(basename $out_relative .md).html"
+          buildPandoc "$file" "$out_relative"
         else
-          # Copy all other files
           cp "$file" $out_relative
         fi
-      done < <(find $src -type f -print0)
+        # -L : Follow symlinks
+        # -type f : Only files, not directories
+        # -print0 : Separate with null bytes
+      done < <(find -L $src -type f -print0)
     '';
   })
