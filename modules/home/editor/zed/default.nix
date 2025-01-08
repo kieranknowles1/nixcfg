1cb30d0430cf0d9ecd80a8549773f42744ae0718
Kieran <kieranknowles11@hotmail.co.uk>
2025-01-08 21:22:49 +0000

    858#rocinante: Update flake inputs
    Generation   Build-date           NixOS version           Kernel  Configuration Revision  Specialisation
    858 current  2025-01-08 21:23:38  25.05.20250107.8c9fd3e  6.6.69                          *
    
    <<< /run/current-system
    >>> result
    Version changes:
    [U.]  #01  carapace                1.1.0 -> 1.1.1
    [C.]  #02  da-one-gray             <none> x6 -> <none> x9
    [U.]  #03  electron                31.7.6 -> 32.2.7
    [U.]  #04  electron-unwrapped      31.7.6 -> 32.2.7
    [U.]  #05  initrd-linux            6.6.68 -> 6.6.69
    [C.]  #06  libXNVCtrl              390.157, 565.77 -> 390.157, 550.142
    [U.]  #07  libportal               0.8.1 -> 0.9.0
    [U.]  #08  linux                   6.6.68, 6.6.68-modules, 6.6.68-modules-shrunk -> 6.6.69, 6.6.69-modules, 6.6.69-modules-shrunk
    [C.]  #09  mesa                    24.0.7 x2, 24.0.7-dev, 24.3.2 x2, 24.3.2-drivers x2, 24.3.2-opencl x2, 24.3.2-osmesa -> 24.0.7 x2, 24.0.7-dev, 24.3.3 x2, 24.3.3-drivers x2, 24.3.3-opencl x2, 24.3.3-osmesa
    [U.]  #10  nixos-system-rocinante  25.05.20241231.69b9a8c -> 25.05.20250107.8c9fd3e
    [D*]  #11  nvidia-settings         565.77 -> 550.142
    [D*]  #12  nvidia-x11              565.77-6.6.68, 565.77-6.6.68-bin, 565.77-6.6.68-firmware, 565.77-6.6.68-lib32 -> 550.142-6.6.69, 550.142-6.6.69-bin, 550.142-6.6.69-lib32
    [U.]  #13  ostree                  2024.8 -> 2024.10
    [U.]  #14  phpstan                 2.0.4 -> 2.1.1
    [U*]  #15  thunar                  4.20.0 -> 4.20.1
    [U*]  #16  thunar-archive-plugin   0.5.2 -> 0.5.3
    [C.]  #17  udev-rules              <none> x2 -> <none>
    [U.]  #18  zed-editor              0.166.2 -> 0.167.2
    [U.]  #19  zed-editor-wrapped      0.166.2 -> 0.167.2
    Added packages:
    [A.]  #1  hm_autostartstylixactivator.desktop  <none>
    [A.]  #2  livekit-libwebrtc-m114-dev           <none>
    [A.]  #3  nerd-fonts-dejavu-sans-mono          3.3.0+2.37
    [A.]  #4  openmw-master                        <none>
    [A.]  #5  sd-switch                            0.5.3
    [A.]  #6  stylix-set-kde-wallpaper             <none>
    Removed packages:
    [R.]  #1  dejavu-sans-mono     2.37
    [R.]  #2  extra-utils          <none>
    [R.]  #3  initrd-fsinfo        <none>
    [R.]  #4  keymap               <none>
    [R.]  #5  link-units           <none>
    [R.]  #6  openmw               0.49.0
    [R.]  #7  stage                1-init.sh
    [R.]  #8  systemd-activate.sh  <none>
    [R.]  #9  treefmt-nix          <none> x2
    Closure size: 3355 -> 3352 (203 paths added, 206 paths removed, delta -3, disk usage -250.3MiB).

--- modules/home/editor/zed/default.nix
+++ modules/home/editor/zed/default.nix
@@ -27,6 +27,9 @@
         message = "Zed requires a desktop environment.";
       };
 
+      # Requires writing zed/settings.json, conflicts with activate-mutable
+      stylix.targets.zed.enable = false;
+
       programs.zed-editor = {
         enable = true;
         # Zed will try to download language servers automatically, but will fail
