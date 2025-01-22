e17dbaa73d376c46e5deb618221996c8ea386815
Kieran <kieranknowles11@hotmail.co.uk>
2025-01-22 00:06:05 +0000

    New wallpaper

--- users/kieran/config-nix.nix
+++ users/kieran/config-nix.nix
@@ -5,7 +5,8 @@
   ...
 }: {
   config.custom = {
-    theme.wallpaper = self.builders.${pkgs.stdenv.hostPlatform.system}.fromHeif ./wallpaper.heic;
+    # TODO: Can we cycle through wallpapers?
+    theme.wallpaper = self.builders.${pkgs.stdenv.hostPlatform.system}.fromHeif ./wallpaper-2.heic;
 
     secrets = {
       ageKeyFile = "/home/kieran/.config/sops/age/keys.txt";
