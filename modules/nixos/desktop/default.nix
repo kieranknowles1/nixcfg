d1c6561b8cca4ec358285c605fd6695634b50a50
Kieran <kieranknowles11@hotmail.co.uk>
2025-01-29 21:40:33 +0000

    Fix eval warning

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -30,7 +30,7 @@
     services.xserver.enable = true;
 
     # Enable sound with pipewire.
-    hardware.pulseaudio.enable = false;
+    services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
