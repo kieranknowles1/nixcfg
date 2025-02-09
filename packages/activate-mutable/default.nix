33e00a52987ccaee135ffc3f0b5a82d42e464a5b
Kieran <kieranknowles11@hotmail.co.uk>
2025-02-09 20:02:46 +0000

    Update cargo inputs + version bump

--- packages/activate-mutable/default.nix
+++ packages/activate-mutable/default.nix
@@ -1,10 +1,10 @@
 {rustPlatform}:
 rustPlatform.buildRustPackage rec {
   pname = "activate-mutable";
-  version = "2.0.2";
+  version = "2.0.3";
   src = ./.;
 
-  cargoHash = "sha256-Por64B903VI8s/PJf1llriILic7BJ6DP8aJabdmKlVw=";
+  cargoHash = "sha256-PKX/nsYkVc4CZ42xrxHgcbGvfilEPDmF9V8Tsf2oKC0=";
 
   meta = {
     description = "Activate mutable files in the home directory";
