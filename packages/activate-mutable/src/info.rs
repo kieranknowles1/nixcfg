d942cb3b10791f846dc659ba1a60ea3b7bc2b696
Kieran <kieranknowles11@hotmail.co.uk>
2025-02-27 00:02:46 +0000

    Display status of file in info subcommand

--- packages/activate-mutable/src/info.rs
+++ packages/activate-mutable/src/info.rs
@@ -1,9 +1,12 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use clap::Parser;
 use thiserror::Error;
 
-use crate::config::{get_previous_config_path, or_environ, read_config};
+use crate::{
+    config::{get_previous_config_path, or_environ, read_config, resolve_directory},
+    state::{hash_file, ExistingMatch},
+};
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -11,6 +14,10 @@ pub enum Error {
     Environ(#[from] std::env::VarError),
     #[error("Config error: {0}")]
     Config(#[from] crate::config::Error),
+    #[error("Directory traversal")]
+    DirectoryTraversal(#[from] crate::config::DirectoryTraversalError),
+    #[error("IO Error")]
+    Io(#[from] std::io::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -19,6 +26,18 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub struct Opt {
     #[clap(long)]
     home: Option<PathBuf>,
+}
+
+fn get_status(home: &Path, store_path: &Path, home_path: &Path) -> Option<&'static str> {
+    let store_hash = hash_file(&store_path).ok()?;
+    let full_home = resolve_directory(home, home_path).ok()?;
+    let home_hash = hash_file(&full_home).ok()?;
+
+    match ExistingMatch::from_hashes(None, store_hash, home_hash) {
+        ExistingMatch::EqualNew => Some("Not changed"),
+        ExistingMatch::Conflict => Some("Changed"),
+        ExistingMatch::EqualOld => panic!("No old hash was passed, how did thi hapen?"),
+    }
 }
 
 pub fn run(args: Opt) -> Result<()> {
@@ -30,9 +49,12 @@ pub fn run(args: Opt) -> Result<()> {
             Some(repo) => repo,
             None => "<no repo path>",
         };
+        let status = get_status(&home, &entry.source, &entry.destination).unwrap_or("Unknown");
+
         println!("{}", entry.destination.display());
         println!("  Repository: {}", repo);
         println!("  On conflict: {:?}", entry.on_conflict);
+        println!("  Status: {}", status);
     }
 
     Ok(())
