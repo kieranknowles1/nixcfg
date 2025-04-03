3f928511a0f898d45da308bd1965e695191fc525
Kieran <kieranknowles11@hotmail.co.uk>
2025-04-03 21:37:01 +0100

    Convert game of life to TypeScript

--- packages/portfolio/src/gameOfLife.ts
+++ packages/portfolio/src/gameOfLife.ts
@@ -1,14 +1,27 @@
 'use strict'
 
+// Get a required element by its ID, throwing if not found
+// Does not verify element type, only that it exists
+function getById<T>(id: string): T {
+    const elem = document.getElementById(id)
+    if (elem == null) {
+        throw new Error(`Could not get element ${id}`)
+    }
+    return elem as T
+}
+
+interface Vec2 { x: number, y: number }
 // Utilities to make working with 2D arrays easier
-function vec2(x, y) { return { x, y }}
-function vecAdd(a, b) {
+function vec2(x: number, y: number): Vec2 { return { x, y }}
+function vecAdd(a: Vec2, b: Vec2) {
     return vec2(a.x + b.x, a.y + b.y)
 }
 
-/** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('gameoflife')
+const canvas = getById<HTMLCanvasElement>('gameoflife')
 const ctx = canvas.getContext('2d')
+if (ctx == null) {
+    throw new Error('Failed to get canvas context')
+}
 
 const UPDATE_FREQUENCY = 250
 const PIXEL_SIZE = 10
@@ -20,9 +33,12 @@ const NEIGHBOUR_OFFSETS = [
     vec2(-1, 1),  vec2(0, 1),  vec2(1, 1)
 ]
 
-const cells = Array.from({ length: GRID_SIZE.x }, (v, i) => Array.from({ length: GRID_SIZE.y }, (v, i) => false))
+const cells = Array.from({ length: GRID_SIZE.x },
+    _ => Array.from({ length: GRID_SIZE.y }, _ => false)
+)
 
-function forEachCell(cb) {
+type CellCallback = (pos: Vec2) => void
+function forEachCell(cb: CellCallback) {
     for (let x = 0; x < GRID_SIZE.x; x++) {
         for (let y = 0; y < GRID_SIZE.y; y++) {
             cb(vec2(x, y))
@@ -31,13 +47,13 @@ function forEachCell(cb) {
 }
 
 // Defaults to `false` for out of bounds locations
-function cellAlive(pos) {
+function cellAlive(pos: Vec2) {
     if (pos.x < 0 || pos.y < 0 || pos.x >= GRID_SIZE.x || pos.y >= GRID_SIZE.y) {
         return false
     }
     return cells[pos.x][pos.y]
 }
-function setCellState(pos, state) {
+function setCellState(pos: Vec2, state: boolean) {
     cells[pos.x][pos.y] = state
 }
 
@@ -65,6 +81,9 @@ function update() {
 }
 
 function render() {
+    // Should be unreachable
+    if (!ctx) throw new Error('Canvas context missing')
+
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     ctx.fillStyle = 'white'
@@ -76,7 +95,7 @@ function render() {
 }
 
 let active = true
-setInterval(_ => {
+setInterval((_: unknown) => {
     if (!active) return;
     update()
     render()
@@ -85,13 +104,14 @@ reset()
 update()
 render()
 
-document.getElementById('gameoflife_reset').addEventListener('click', _ => {
+getById<HTMLButtonElement>('gameoflife_reset').addEventListener('click', _ => {
     reset()
     update()
     render()
 })
 
-document.getElementById('gameoflife_pause').addEventListener('click', event => {
+const pauseButton = getById<HTMLButtonElement>('gameoflife_pause')
+pauseButton.addEventListener('click', event => {
     active = !active
-    event.target.textContent = active ? 'Pause' : 'Unpause'
+    pauseButton.textContent = active ? 'Pause' : 'Unpause'
 })
