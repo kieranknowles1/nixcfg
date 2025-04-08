f7eff6a9a8e9ab0969807500fe9dbb859b42c47e
Kieran <kieranknowles11@hotmail.co.uk>
2025-04-08 23:26:36 +0100

    checks: simplify creation
    Pass every check exactly one argument for the flake path, and require
    that its name matches the script name

--- checks/default.nix
+++ checks/default.nix
@@ -2,37 +2,17 @@
   perSystem = {pkgs, ...}: {
     checks = let
       # Make a check that will succeed if the script exits 0, and fail otherwise.
-      mkCheck = {
-        nativeBuildInputs ? [],
-        script,
-        name,
-        args ? [],
-      }:
-        pkgs.runCommand name {inherit nativeBuildInputs;} ''
+      # The path to the flake is passed as the first argument to the script.
+      mkCheck = name: nativeBuildInputs:
+        pkgs.runCommand "check-${name}" {inherit nativeBuildInputs;} ''
           set -euo pipefail
-          bash ${script} ${builtins.concatStringsSep " " args}
+          bash ${./${name}.sh} ${self}
           touch $out
         '';
     in {
-      bash-sanity = mkCheck {
-        script = ./bash-sanity.sh;
-        name = "check-bash-sanity";
-        args = [self];
-      };
-
-      duplicate-input = mkCheck {
-        nativeBuildInputs = with pkgs; [jq];
-        script = ./duplicate-input.sh;
-        name = "check-duplicate-input";
-        args = ["${self}/flake.lock"];
-      };
-
-      markdown-links = mkCheck {
-        nativeBuildInputs = with pkgs; [lychee];
-        script = ./markdown-links.sh;
-        name = "check-markdown-links";
-        args = [self];
-      };
+      bash-sanity = mkCheck "bash-sanity" [];
+      duplicate-input = mkCheck "duplicate-input" [pkgs.jq];
+      markdown-links = mkCheck "markdown-links" [pkgs.lychee];
     };
   };
 }
--- checks/duplicate-input.sh
+++ checks/duplicate-input.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-lock_file="$1"
+lock_file="$1/flake.lock"
 
-# Very simple check for duplicate inputs, Nix names these with _<number>, so we can just check for those
-# convention is to use hyphens, so this shouldn't have false positives
+# Very simple check for duplicate inputs, Nix names these with _<number>,
+# while the convention is to use hyphens. Shouldn't cause false positives unless
+# something violates this convention.
 dupes=$(jq '.nodes | with_entries(select(.key|match("_";""))) | keys[]' < "$lock_file")
 
 if [ -n "$dupes" ]; then
