145942dfe1e5a225fbacb7bd2b98c788767ba65c
Kieran <kieranknowles11@hotmail.co.uk>
2025-04-08 23:18:34 +0100

    checks: replace markdown-link-check with lychee
    this is much faster and easier to configure for my needs

--- checks/default.nix
+++ checks/default.nix
@@ -1,8 +1,4 @@
-{
-  self,
-  lib,
-  ...
-}: {
+{self, ...}: {
   perSystem = {pkgs, ...}: {
     checks = let
       # Make a check that will succeed if the script exits 0, and fail otherwise.
@@ -31,30 +27,12 @@
         args = ["${self}/flake.lock"];
       };
 
-      markdown-links = let
-        patch = pkgs.fetchurl {
-          # Appending ".patch" to a PR URL will give you a patch file. Once the PR is merged,
-          # The patch should fail to apply, and you can remove it.
-          url = "https://github.com/tcort/markdown-link-check/pull/372.patch";
-          sha256 = "sha256-Yx/NWUkYFR4Dein4UQs5NLqFp7Ys3mAnShjouwWorE0=";
-        };
-
-        link-check = pkgs.nodePackages.markdown-link-check.overrideAttrs (old: {
-          patches = (old.patches or []) ++ [patch];
-        });
-      in
-        # https://github.com/tcort/markdown-link-check/pull/372
-        # When this assertion fails, see if the above PR has been merged, then either remove the patch or update the version.
-        assert lib.asserts.assertMsg (pkgs.markdown-link-check.version == "3.13.6") "Check if markdown-link-check patch is still needed";
-          mkCheck {
-            nativeBuildInputs = [link-check];
-            script = ./markdown-links.sh;
-            name = "check-markdown-links";
-            # Don't check links to external sites to keep the check as a pure function.
-            # Also ignore files in ./docs/generated, as they are assumed to be OK.
-            # TODO: Could we link the generated files into ./docs/generated, so that they are checked? Which host should they come from?
-            args = [./markdown-link-config.json self];
-          };
+      markdown-links = mkCheck {
+        nativeBuildInputs = with pkgs; [lychee];
+        script = ./markdown-links.sh;
+        name = "check-markdown-links";
+        args = [self];
+      };
     };
   };
 }
--- checks/markdown-links.sh
+++ checks/markdown-links.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cfg_file="$1"
-directory="$2"
+directory="$1"
 
-# All markdown files
-readarray -d '' files < <(find "$directory" -name "*.md" -print0)
-
-# Only print file names with broken links and their broken links
 # TODO: SVGs are only generated at build time. Can we check for
 # a matching .dot file?
-markdown-link-check --quiet --config "$cfg_file" "${files[@]}"
+
+# Exclude links to generated files, as these are not available during checks
+lychee "$directory" --offline --exclude "$directory/docs/generated/" --exclude '\.svg$'
