f8ff242c431e00447e76035a345c250245167692
Kieran <kieranknowles11@hotmail.co.uk>
2025-04-26 15:33:09 +0100

    fix build

--- modules/home/docs.nix
+++ modules/home/docs.nix
@@ -197,11 +197,11 @@
       custom.shortcuts.palette.actions = lib.singleton {
         description = "View documentation";
         # These are built from markdown where the convention is `readme.md` rather than `index.html`
-        action = ["xdg-open" "${cfg.build.combined}/readme.html"];
+        action = ["xdg-open" "${cfg.build.combined.html}/index.html"];
       };
 
       home.file."${config.custom.repoPath}/docs/generated" = {
-        source = "${cfg.build.generated.md}/generated";
+        source = "${cfg.build.generated}/generated";
         recursive = true;
       };
     };
