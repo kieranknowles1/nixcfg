36b1b6de10b829abb9f416097fb05d56e47261fd
Kieran <kieranknowles11@hotmail.co.uk>
2025-05-05 14:07:44 +0100

    vr: add libsurvive and opencomposite
    Still not working fully, but gets Godot pushing to the headset

--- modules/nixos/vr.nix
+++ modules/nixos/vr.nix
@@ -1,7 +1,7 @@
-# https://wiki.nixos.org/wiki/VR
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.custom.vr = let
@@ -14,9 +14,28 @@
     cfg = config.custom.vr;
   in
     lib.mkIf cfg.enable {
+      # OpenXR server
       services.monado = {
         enable = true;
         defaultRuntime = true;
       };
+
+      # Translate OpenVR to OpenXR
+      home-manager.sharedModules = lib.singleton ({config, ...}: {
+        xdg.configFile."openvr/openvrpaths.vrpath".text = builtins.toJSON {
+          config = ["${config.xdg.dataHome}/Steam/config"];
+          external_drivers = null;
+          jsonid = "vrpathreg";
+          log = ["${config.xdg.dataHome}/Steam/logs"];
+          runtime = ["${pkgs.opencomposite}/lib/opencomposite"];
+          version = 1;
+        };
+      });
+
+      environment.systemPackages = with pkgs; [
+        # Lighthouse based tracking
+        libsurvive
+        opencomposite
+      ];
     };
 }
