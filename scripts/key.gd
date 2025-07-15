24eeb99a210b9e03d17f24480834aeee938677b0
Kieran <kieranknowles11@hotmail.co.uk>
2025-07-15 23:49:32 +0100

    display hotkeys if modifiers held

--- scripts/key.gd
+++ scripts/key.gd
@@ -4,8 +4,13 @@ class KeyData:
 	var code: int
 	var lower: String
 	var upper: String
+	
+class HotKey:
+	var modifiers: Data.ModCombo
+	var icon: Texture2D
 
 var data: KeyData
+var hotkeys: Array
 
 @onready var label: Label = $Label
 @onready var texture_rect: TextureRect = $TextureRect
@@ -13,9 +18,19 @@ var data: KeyData
 func _ready():
 	label.text = data.lower
 
+func find_hotkey(modifiers: Data.ModCombo) -> HotKey:
+	for hk in hotkeys:
+		if hk.modifiers == modifiers:
+			return hk
+	return null
+
 func set_modifiers(modifiers: Data.ModCombo):
-	# TODO: Icon for shortcuts
-
-	label.text = data.upper if modifiers & Data.ModCombo.Shift else data.lower
-
-	pass
+	var hk = find_hotkey(modifiers)
+	if hk:
+		texture_rect.texture = hk.icon
+		label.visible = false
+		texture_rect.visible = true
+	else:
+		label.text = data.upper if modifiers & Data.ModCombo.Shift else data.lower
+		label.visible = true
+		texture_rect.visible = false
