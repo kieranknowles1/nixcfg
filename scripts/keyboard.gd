9acfe85f90891208d3207960d28c3b374bd69060
Kieran <kieranknowles11@hotmail.co.uk>
2025-07-15 23:36:46 +0100

    remove unused code

--- scripts/keyboard.gd
+++ scripts/keyboard.gd
@@ -1,13 +1,12 @@
 class_name Keyboard extends Node
 
+@onready var config: Config = $"../Config"
+
 const KEY = preload("res://prefabs/key.tscn")
 
 const LOWER: Array[String] = ["1234567890-=", "qwertyuiop[]", "asdfghjkl;'#", "\\zxcvbnm,./"]
 const UPPER: Array[String] = ['!"£$%^&*()_+', "QWERTYUIOP{}", "ASDFGHJKL:@~", "|ZXCVBNM<>?"]
 
-# TODO: Rest of the keyboard
-#const LOWER = "qwertyuiop[] asdfghjkl;'# \\zxcvbnm<>/"
-#const UPPER = "QWERTYUIOP{} ASDFGHJKL:@~ qwertyuiop[]|asdfghjkl;'#|\\zxcvbnm<>/"
 const PADDING = [0, 10, 42, 0]
 
 var keys: Array[Key] = []
@@ -22,14 +21,18 @@ func new_row(pad: int = 0):
 	add_child(row)
 	return row
 
+
+
 func _ready():
+	# Load all modifiers
+	var cfg = config.load_json(config.get_config_path())
+	
 	for i in range(0, LOWER.size()):
 		var lower = LOWER[i]
 		var upper = UPPER[i]
 		
 		var row = new_row(PADDING[i])
 		for key in range(0, lower.length()):
-			#var code = OS.find_keycode_from_string(key)
 			var instance: Key = KEY.instantiate()
 			instance.data = Key.KeyData.new()
 			instance.data.lower = lower.substr(key, 1)
@@ -38,25 +41,6 @@ func _ready():
 			row.add_child(instance)
 	#
 			keys.append(instance)
-	#var index = 0
-	#var row = new_row(PADDING[0])
-	#index += 1
-	#for key in LAYOUT.split(""):
-		#if key == '|':
-			#row = new_row(PADDING[index])
-			#index += 1
-			#continue
-#
-		#var code = OS.find_keycode_from_string(key)
-		#var instance: Key = KEY.instantiate()
-#
-		#instance.data = Key.KeyData.new()
-		#instance.data.code = code
-		#instance.data.lower = key
-		#instance.data.upper = key.to_upper()
-		#row.add_child(instance)
-#
-		#keys.append(instance)
 
 func _input(_event: InputEvent) -> void:
 	var mod = Data.get_modifiers()
