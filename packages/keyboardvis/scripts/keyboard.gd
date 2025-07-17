96e3df83db660101fbc3780a974231094a40dcea
Kieran <kieranknowles11@hotmail.co.uk>
2025-07-17 21:50:28 +0100

    keyboardvis: handle slash key

--- packages/keyboardvis/scripts/keyboard.gd
+++ packages/keyboardvis/scripts/keyboard.gd
@@ -7,6 +7,11 @@ const KEY = preload("res://prefabs/key.tscn")
 const LOWER: Array[String] = ["1234567890-=", "qwertyuiop[]", "asdfghjkl;'#", "\\zxcvbnm,./"]
 const UPPER: Array[String] = ['!"£$%^&*()_+', "QWERTYUIOP{}", "ASDFGHJKL:@~", "|ZXCVBNM<>?"]
 
+# Keys that have different names to their label
+const ALTERNATE_NAMES: Dictionary = {
+	"/": "slash"
+}
+
 const PADDING = [0, 10, 32, 0]
 
 @export var keys: Array[KeyNode] = []
@@ -44,9 +49,11 @@ func _ready():
 			row.add_child(instance)
 			keys.append(instance)
 	
+	# Include pre-set keys
 	for instance in keys:
-		if instance.data.lower in cfg:
-			instance.hotkeys = cfg[instance.data.lower]
+		var code = ALTERNATE_NAMES[instance.data.lower] if instance.data.lower in ALTERNATE_NAMES else instance.data.lower
+		if code in cfg:
+			instance.hotkeys = cfg[code]
 
 
 func _input(_event: InputEvent) -> void:
