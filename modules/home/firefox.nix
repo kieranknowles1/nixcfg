8cfc62c4d3b28107e592f0fc2cba00cebee15706
Kieran <kieranknowles11@hotmail.co.uk>
2025-08-01 16:38:17 +0100

    firefox: don't install pdf mime associations if not enabling firefox

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -23,71 +23,72 @@
 
   config = let
     cfg = config.custom.firefox;
-  in {
-    programs.firefox = {
-      enable = hostConfig.custom.features.desktop;
+  in
+    lib.mkIf hostConfig.custom.features.desktop {
+      programs.firefox = {
+        enable = true;
 
-      policies = {
-        AutofillCreditCardEnabled = false;
-        # Updates are managed by Nix
-        DisableAppUpdate = true;
-        DisableTelemetry = true;
-        # Extensions are also managed
-        ExtensionUpdate = false;
-        # My data is none of your business
-        EnableTrackingProtection = {
-          Value = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-          EmailTracking = true;
-          Locked = true;
+        policies = {
+          AutofillCreditCardEnabled = false;
+          # Updates are managed by Nix
+          DisableAppUpdate = true;
+          DisableTelemetry = true;
+          # Extensions are also managed
+          ExtensionUpdate = false;
+          # My data is none of your business
+          EnableTrackingProtection = {
+            Value = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+            EmailTracking = true;
+            Locked = true;
+          };
+          # I use Bitwarden for this
+          PasswordManagerEnabled = false;
+          # Don't use this
+          DisablePocket = true;
+          DisableFirefoxAccounts = true;
         };
-        # I use Bitwarden for this
-        PasswordManagerEnabled = false;
-        # Don't use this
-        DisablePocket = true;
-        DisableFirefoxAccounts = true;
+
+        profiles.default = {
+          id = 0;
+          name = "default";
+          isDefault = true;
+
+          # To search available extensions, run
+          # `nix flake show gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons | grep <search-term>`
+          extensions = with pkgs.firefox-addons;
+            [
+              bitwarden # Password manager. Available everywhere
+              # TODO: Manage config for extensions. How do we export?
+              consent-o-matic # Do you consent to being tracked by 1000 companies? No? Too bad.
+              darkreader # Midnight flashbang blocker
+              privacy-badger # My activity is none of your business
+              return-youtube-dislikes # The news doesn't want you to know what other people think
+              indie-wiki-buddy # Redirect *.fandom.com to non-fandom wikis
+              sponsorblock # This configuration is sponsored by Raid: Sha... oh no, it's spreading
+              # TODO: Auto install https://github.com/laylavish/uBlockOrigin-HUGE-AI-Blocklist
+              ublock-origin # HAVE YOU HEARD OF THIS PRODUCT YOU WANT TO BUY? BUY IT NOW
+              youtube-shorts-block # No TikTok in this house
+            ]
+            ++ cfg.extraExtensions;
+
+          settings = {
+            # Enable extensions automatically
+            "extensions.autoDisableScopes" = 0;
+
+            # Disable built-in advertising
+            "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          };
+        };
       };
 
-      profiles.default = {
-        id = 0;
-        name = "default";
-        isDefault = true;
-
-        # To search available extensions, run
-        # `nix flake show gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons | grep <search-term>`
-        extensions = with pkgs.firefox-addons;
-          [
-            bitwarden # Password manager. Available everywhere
-            # TODO: Manage config for extensions. How do we export?
-            consent-o-matic # Do you consent to being tracked by 1000 companies? No? Too bad.
-            darkreader # Midnight flashbang blocker
-            privacy-badger # My activity is none of your business
-            return-youtube-dislikes # The news doesn't want you to know what other people think
-            indie-wiki-buddy # Redirect *.fandom.com to non-fandom wikis
-            sponsorblock # This configuration is sponsored by Raid: Sha... oh no, it's spreading
-            # TODO: Auto install https://github.com/laylavish/uBlockOrigin-HUGE-AI-Blocklist
-            ublock-origin # HAVE YOU HEARD OF THIS PRODUCT YOU WANT TO BUY? BUY IT NOW
-            youtube-shorts-block # No TikTok in this house
-          ]
-          ++ cfg.extraExtensions;
-
-        settings = {
-          # Enable extensions automatically
-          "extensions.autoDisableScopes" = 0;
-
-          # Disable built-in advertising
-          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+      # Open PDFs in Firefox
+      custom.mime.definition = {
+        "application/pdf" = {
+          defaultApp = "firefox.desktop";
         };
       };
     };
-
-    # Open PDFs in Firefox
-    custom.mime.definition = {
-      "application/pdf" = {
-        defaultApp = "firefox.desktop";
-      };
-    };
-  };
 }
