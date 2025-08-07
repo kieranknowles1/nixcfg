fbaf1749355946b1f2ee9b661e9532f7ba34506f
Kieran <kieranknowles11@hotmail.co.uk>
2025-08-07 23:06:22 +0100

    search: initial search module

---
+++ modules/nixos/server/search.nix
@@ -0,0 +1,75 @@
+{
+  lib,
+  config,
+  self,
+  inputs,
+  pkgs,
+  ...
+}: {
+  options.custom.server.search = let
+    inherit (lib) mkOption mkEnableOption types;
+    mkModuleOption = name:
+      mkOption {
+        type = types.anything;
+
+        description = "${name} options to generate search for. Defaults to the nixcfg flake's default module.";
+      };
+  in {
+    enable = mkEnableOption "Options Search";
+
+    subdomain = mkOption {
+      type = types.str;
+      default = "search";
+      description = ''
+        The subdomain to use for options search.
+      '';
+    };
+
+    nixosModules = mkModuleOption "NixOS";
+    homeModules = mkModuleOption "Home Manager";
+
+    githubPath = mkOption {
+      type = types.str;
+      default = "https://github.com/kieranknowles1/nixcfg/";
+      description = ''
+        The base URL of the configuration's GitHub repository.
+      '';
+    };
+  };
+
+  config = let
+    inherit (inputs.nuschtosSearch.packages.${pkgs.system}) mkMultiSearch;
+
+    cfg = config.custom.server;
+    cfgs = cfg.search;
+
+    suffixUrl = suffix: "https://${cfgs.subdomain}.${cfg.hostname}/${suffix}/";
+  in
+    lib.mkIf cfgs.enable {
+      custom.server.search = {
+        nixosModules = self.nixosModules.default;
+        homeModules = self.homeManagerModules.default;
+      };
+
+      custom.server.subdomains = {
+        ${cfgs.subdomain} = {
+          cache.enable = true;
+          root = mkMultiSearch {
+            baseHref = "/";
+            scopes = [
+              {
+                title = "NixOS Modules";
+                modules = [cfgs.nixosModules];
+                urlPrefix = suffixUrl "nixos";
+              }
+              {
+                title = "Home Manager Modules";
+                modules = [cfgs.homeModules];
+                urlPrefix = suffixUrl "home";
+              }
+            ];
+          };
+        };
+      };
+    };
+}
