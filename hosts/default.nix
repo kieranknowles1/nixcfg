6b83283b774c9f4018ea7e4d363622d71c9aef6f
Kieran <kieranknowles11@hotmail.co.uk>
2025-08-10 00:19:13 +0100

    57#tycho: server: enable extras
    these don't use rpi optimised overlays. oh well
    
    Generation  Build-date           NixOS version                                                                     Kernel   Configuration Revision  Specialisation  Current
    57          2025-08-10 00:16:52  25.11.20250808.a4c3cf3                                                            6.12.25  Unknown                 []              True
    
    <<< /run/current-system
    >>> result
    Added packages:
    [A.]  #1  lazygit                     0.54.0
    [A.]  #2  lazygit-config              <none>
    [A.]  #3  starship                    1.23.0
    [A.]  #4  starship-config             <none>
    [A.]  #5  starship-nushell-config.nu  <none>
    Closure size: 767 -> 772 (20 paths added, 15 paths removed, delta +5, disk usage +34.4MiB).

--- hosts/default.nix
+++ hosts/default.nix
@@ -5,7 +5,7 @@
 }: let
   inherit (self.lib.host) mkHost;
   mkDefaultHost = mkHost inputs.nixpkgs.lib.nixosSystem {};
-  mkRpiHost = mkHost inputs.nixos-raspberrypi.lib.nixosSystemFull {inherit (inputs) nixos-raspberrypi;};
+  mkRpiHost = mkHost inputs.nixos-raspberrypi.lib.nixosSystem {inherit (inputs) nixos-raspberrypi;};
 in {
   flake.nixosConfigurations = {
     canterbury = mkDefaultHost ./canterbury/configuration.nix;
