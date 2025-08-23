71211e66ca879ce05b5073568737240af0c99bef
Kieran <kieranknowles11@hotmail.co.uk>
2025-08-23 14:40:45 +0100

    homepage: add base widgets

--- modules/nixos/server/homepage.nix
+++ modules/nixos/server/homepage.nix
@@ -5,12 +5,23 @@
 }: {
   options.custom.server.homepage = let
     inherit (lib) mkOption mkEnableOption types;
+
+    widgetType = types.submodule {
+      options = {
+      };
+    };
   in {
     enable = mkEnableOption "gethomepage";
     subdomain = mkOption {
       type = types.str;
       default = "home";
       description = "The subdomain for gethomepage";
+    };
+
+    widgets = mkOption {
+      type = types.listOf widgetType;
+      default = [];
+      description = "List of widgets to display on the homepage";
     };
   };
 
@@ -26,7 +37,44 @@
       services.homepage-dashboard = {
         enable = true;
         listenPort = cfg.ports.tcp.homepage;
-        allowedHosts = cfg.hostname;
+        allowedHosts = "${cfgh.subdomain}.${cfg.hostname}";
+
+        widgets = [
+          {
+            datetime = {
+              locale = "en-GB"; # Day-month-year is objectively wrong
+              format = {
+                dateStyle = "medium";
+                timeStyle = "short";
+              };
+            };
+          }
+          {
+            resources = {
+              label = "System";
+              cpu = true;
+              memory = true;
+              cputemp = true;
+              tempmin = 0;
+              tempmax = 100;
+              units = "metric";
+              refresh = 5000;
+              network = true;
+            };
+          }
+          {
+            resources = {
+              label = "Internal";
+              disk = "/";
+            };
+          }
+          {
+            resources = {
+              label = "External";
+              disk = "/mnt/extern";
+            };
+          }
+        ];
       };
     };
 }
