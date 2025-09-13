0becb3dd0dbe39b3870c681f9bd8f945f3be9cca
Kieran <kieranknowles11@hotmail.co.uk>
2025-09-13 22:53:07 +0100

    factorio: remove blueprint decoder
    this doesn't work for 2.0,  community doesn't seem to have updated it
    and I don't have the time to do so myself

--- shells/defaultShell.nix
+++ shells/defaultShell.nix
@@ -1,37 +1,23 @@
 {
-  lib,
-  writeShellScriptBin,
   mkShellNoCC,
   nil,
   flake,
   python312,
-}: let
-  openmw-luadata = lib.getExe flake.openmw-luadata;
+}:
+flake.lib.shell.mkShellEx mkShellNoCC {
+  name = "meta";
 
-  export-openmw = writeShellScriptBin "export-openmw" ''
-    config_dir="$HOME/.config/openmw"
+  packages = [
+    flake.rebuild
+    nil
 
-    ${openmw-luadata} decode "$config_dir/global_storage.bin" > "$FLAKE/users/kieran/openmw/global_storage.json"
-    ${openmw-luadata} decode "$config_dir/player_storage.bin" > "$FLAKE/users/kieran/openmw/player_storage.json"
+    # Used by [[../modules/home/espanso/patch-matches.py]]
+    (python312.withPackages (python-pkgs: [
+      python-pkgs.pyyaml
+    ]))
+  ];
+
+  shellHook = ''
+    cd "$FLAKE"
   '';
-in
-  flake.lib.shell.mkShellEx mkShellNoCC {
-    name = "meta";
-
-    packages = [
-      flake.export-blueprints
-      flake.factorio-blueprint-decoder
-      flake.rebuild
-      nil
-      export-openmw
-
-      # Used by [[../modules/home/espanso/patch-matches.py]]
-      (python312.withPackages (python-pkgs: [
-        python-pkgs.pyyaml
-      ]))
-    ];
-
-    shellHook = ''
-      cd "$FLAKE"
-    '';
-  }
+}
