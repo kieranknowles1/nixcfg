ecc80d4dd31f734b73e39dd82ca5fc84fa00f7a9
Kieran <kieranknowles11@hotmail.co.uk>
2025-09-28 19:47:46 +0100

    homepage: fix .local rewrite
    Old version required a trailing slash, fixed to work if this is missing
    or on HTTPs connections

--- modules/nixos/server/homepage/custom.js
+++ modules/nixos/server/homepage/custom.js
@@ -5,7 +5,7 @@ const PUBLIC_DOMAIN = 'selwonk.uk'
 
 // Rewrite *.domain.com links to *.hostname.local IF we're using hostname.local,
 // and drop SSL due to the lack of a certificate
-if (window.location.href == `http://${LOCAL_DOMAIN}/`) {
+if (window.location.href.includes(LOCAL_DOMAIN)) {
   document.querySelectorAll('a').forEach(link => {
     link.href = link.href
       .replace(PUBLIC_DOMAIN, LOCAL_DOMAIN)
