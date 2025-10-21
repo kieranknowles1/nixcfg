54d8991f19cefdfbcb087388861116cb13597a56
Kieran <kieranknowles11@hotmail.co.uk>
2025-10-21 23:07:28 +0100

    actual: use main data directory
    set data directory properly, nixos uses /var/lib by default which is not
    backed up

--- modules/nixos/server/actual.nix
+++ modules/nixos/server/actual.nix
@@ -9,9 +9,15 @@
     enable = mkEnableOption "Actual Finance";
 
     subdomain = mkOption {
-      type = types.string;
+      type = types.str;
       default = "finance";
       description = "The subdomain for Actual";
+    };
+
+    dataDir = mkOption {
+      type = types.path;
+      defaultText = "$${config.custom.server.data.baseDirectory}/actual";
+      description = "The directory where Actual will store its data";
     };
   };
 
@@ -20,14 +26,46 @@
     cfga = cfg.actual;
   in
     lib.mkIf cfga.enable {
-      custom.server.subdomains = {
-        ${cfga.subdomain}.proxyPort = cfg.ports.tcp.actual;
+      custom.server = {
+        subdomains.${cfga.subdomain} = {
+          proxyPort = cfg.ports.tcp.actual;
+        };
+
+        actual.dataDir = "${cfg.data.baseDirectory}/actual";
+      };
+      custom.servicedir.${cfga.dataDir} = {
+        user = "actual";
+        group = "actual";
+      };
+
+      systemd.services.actual.serviceConfig = {
+        # A dynamic user can't properly own files outside of /var
+        DynamicUser = lib.mkForce false;
+        User = "actual";
+        Group = "actual";
+        # Allow Actual to write to its data directory
+        # nixpkgs sets ProtectSystem=strict, which mounts most
+        # directories read-only. Whitelist only what Actual needs.
+        ReadWritePaths = [ cfga.dataDir ];
+      };
+      users.groups.actual = {};
+      users.users.actual = {
+        isSystemUser = true;
+        group = "actual";
       };
 
       services.actual = {
         enable = true;
-        settings = {
+        # See https://actualbudget.org/docs/config/
+        settings = rec {
+          inherit (cfga) dataDir;
+
           port = cfg.ports.tcp.actual;
+
+          # SQLite Database directory
+          serverFiles = "${dataDir}/server-files";
+          # User data
+          userFiles = "${dataDir}/user-files";
         };
       };
     };
