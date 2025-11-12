28e2adbb0415e95017a9b5fbdd7baae6f9af0470
Kieran <kieranknowles11@hotmail.co.uk>
2025-11-12 14:55:12 +0000

    homepage: env for port

--- packages/homepage-back/src/main.rs
+++ packages/homepage-back/src/main.rs
@@ -1,6 +1,8 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::OnceLock;
 
+use clap::Parser;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
@@ -9,6 +11,17 @@ use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
+#[derive(clap::Parser)]
+struct Cli {
+    #[clap(long, env = "HOMEPAGE_PORT", default_value = "3000")]
+    port: u16,
+}
+
+static CLI: OnceLock<Cli> = OnceLock::new();
+fn cli() -> &'static Cli {
+    CLI.get_or_init(|| Cli::parse())
+}
+
 async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
@@ -16,7 +29,7 @@ async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Listen on port 3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], cli().port));
     let listener = TcpListener::bind(addr).await?;
 
     loop {
