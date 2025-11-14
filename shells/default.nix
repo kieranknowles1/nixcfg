41325fcfb2c4cc3aad3641021f6b068b8ec6ec8f
Kieran <kieranknowles11@hotmail.co.uk>
2025-11-14 23:25:23 +0000

    homepage: add dev shell

--- shells/default.nix
+++ shells/default.nix
@@ -2,10 +2,11 @@
   perSystem = {pkgs, ...}: let
     inherit (pkgs) callPackage;
   in {
-    devShells = {
+    devShells = rec {
       # `default.nix` is already used for this file, so use a different name
       default = callPackage ./defaultShell.nix {};
       cmake = callPackage ./cmake.nix {};
+      homepage = callPackage ./homepage.nix {inherit rust;};
       rust = callPackage ./rust.nix {};
     };
   };
---
+++ shells/homepage.nix
@@ -0,0 +1,42 @@
+{
+  nodejs,
+  rust,
+  tmux,
+  writeShellScriptBin,
+}:
+rust.overrideAttrs (oldAttrs: let
+  nodeLaunch = writeShellScriptBin "node-launch" ''
+    cd $FLAKE/packages/homepage-front
+    npm run dev
+  '';
+
+  rustLaunch = writeShellScriptBin "rust-launch" ''
+    cd $FLAKE/packages/homepage-back
+
+    export HOMEPAGE_ENABLE_SYSINFO=true
+    cargo run
+  '';
+
+  devBoth = writeShellScriptBin "launch-all" ''
+    tmux new-session -d -s homepage rust-launch
+    tmux split-window -h node-launch
+
+    tmux attach-session -t homepage
+  '';
+in {
+  name = "homepage";
+
+  nativeBuildInputs =
+    oldAttrs.nativeBuildInputs
+    ++ [
+      nodejs
+      tmux
+      nodeLaunch
+      rustLaunch
+      devBoth
+    ];
+
+  shellHook = ''
+    launch-all
+  '';
+})
