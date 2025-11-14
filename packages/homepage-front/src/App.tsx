78044f673eaf07f6eeabe6fe619f36ba5f8250b0
Kieran <kieranknowles11@hotmail.co.uk>
2025-11-14 14:14:19 +0000

    homepage: use new endpoint

--- packages/homepage-front/src/App.tsx
+++ packages/homepage-front/src/App.tsx
@@ -1,37 +1,41 @@
 import type { CombinedResponse } from "./bindings/CombinedResponse"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import useInterval from "./useInterval"
 import Metrics from "./Metrics"
+import { type EnabledMetrics } from "./bindings/EnabledMetrics"
 
 const API_ROOT = "http://localhost:4321"
 const UPDATE_INTERVAL = 1000
 const SAMPLES = 15
 
 function App() {
+  const [enabled, setEnabled] = useState<EnabledMetrics>()
   const [metrics, setMetrics] = useState<CombinedResponse[]>([])
 
+  function upsertMetric(data: CombinedResponse) {
+    const newMetrics = [...metrics, data]
+    if (newMetrics.length > SAMPLES) newMetrics.shift()
+
+    setMetrics(newMetrics)
+  }
+
+  useEffect(() => {
+    fetch(`${API_ROOT}/info`)
+      .then(res => res.json() as Promise<EnabledMetrics>)
+      .then(setEnabled)
+  })
+
   useInterval(() => {
-    fetch(API_ROOT)
-      .then(res => res.json() as CombinedResponse)
-      .then(data => {
-        const newMetrics = [...metrics, data]
-        if (newMetrics.length > SAMPLES) newMetrics.shift()
-
-        setMetrics(newMetrics)
-      })
-      .catch(e => {
-        const newMetrics = [...metrics, {}]
-        if (newMetrics.length > SAMPLES) newMetrics.shift()
-        setMetrics(newMetrics)
-      })
+    fetch(`${API_ROOT}/metrics`)
+      .then(res => res.json() as Promise<CombinedResponse>)
+      .then(upsertMetric)
+      // If the server fails to respond, add a period of no data
+      .catch(_ => { upsertMetric({}) })
   }, UPDATE_INTERVAL, true)
-
-  // TODO: Endpoint to check what's available
-  const baseline = metrics.length > 0 ? metrics[0] : {}
 
   return (
     <>
-      {baseline.sysinfo && <Metrics samples={metrics.map(m => m.sysinfo)} />}
+      {enabled?.sysinfo && <Metrics samples={metrics.map(m => m.sysinfo)} />}
     </>
   )
 }
