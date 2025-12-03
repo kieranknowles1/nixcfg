6f6a7ca91d81fc439a93a500c8637766926bc6d6
Kieran <kieranknowles11@hotmail.co.uk>
2025-12-03 00:03:26 +0000

    nvim: enable telescope, which-key, binding for lazygit

--- packages/nvim/editor.nix
+++ packages/nvim/editor.nix
@@ -1,5 +1,21 @@
 {
   vim = {
     options.shiftwidth = 2;
+
+    telescope = {
+      enable = true;
+    };
+
+    binds.whichKey = {
+      enable = true;
+    };
+
+    keymaps = [
+      {
+        key = "<leader>g";
+        mode = "n";
+        action = ":terminal lazygit<CR>i";
+      }
+    ];
   };
 }
