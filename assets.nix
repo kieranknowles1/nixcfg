babe6b57975471be0c5a74abcc5da05f5a047174
Kieran <kieranknowles11@hotmail.co.uk>
2025-12-03 01:08:50 +0000

    assets: fetch assets as flake output

--- assets.nix
+++ assets.nix
@@ -9,39 +9,22 @@
   in
     mkSubmoduleOptions {
       assets = mkOption {
-        type = types.anything;
+        type = types.lazyAttrsOf types.path;
         default = {};
-        description = "Third-party assets for the flake.";
+        description = "External assets for the flake.";
       };
     };
 
   config.flake.assets = let
-    fetchGitHub = {
-      owner,
-      repo,
-      rev,
-      hash,
-    }:
-      builtins.fetchTarball {
-        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+    manifest = builtins.fromJSON (builtins.readFile ./asset-manifest.json);
+    owner = "kieranknowles1";
+    repo = "nixcfg-assets";
+    url = path: "https://raw.githubusercontent.com/${owner}/${repo}/${manifest.rev}/${path}";
+  in
+    lib.mapAttrs (path: hash:
+      builtins.fetchurl {
+        url = url path;
         sha256 = hash;
-      };
-  in {
-    # Like these for generic icons
-    mdi-icons = fetchGitHub {
-      owner = "Templarian";
-      repo = "MaterialDesign";
-      # Master as of 19/12/24
-      rev = "ce55b68ba7308fef54003d5c588343eeac30ff7a";
-      hash = "sha256-S5EugaVJpFxLYM6s+Ujd8vyD6MUa+sxwQrBGTT+ve6w=";
-    };
-
-    # Brand icons that are deliberately excluded from the Material Design set
-    simple-icons = fetchGitHub {
-      owner = "simple-icons";
-      repo = "simple-icons";
-      rev = "13.21.0";
-      hash = "sha256-hBb4jIGxdlNE/Om1cpPYHpw4YSD/kkYOdZpXr63wM+w=";
-    };
-  };
+      })
+    manifest.files;
 }
