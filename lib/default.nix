4ed00ee20c0d410793cad22fb94f131d8bc632bd
Kieran <kieranknowles11@hotmail.co.uk>
2026-01-23 00:52:29 +0000

    license: switch to agpl3-only

--- lib/default.nix
+++ lib/default.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   flake.lib = {
-    license = lib.licenses.mit;
+    license = lib.licenses.agpl3Only;
     docs = import ./docs.nix {inherit lib;};
     host = import ./host.nix {inherit self inputs;};
     shell = import ./shell.nix;
